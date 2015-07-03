9cfd48bd5c5a52817c21d114b86fca2fae616553
Nils van Velzen <nilsvanvelzen@gmail.com>
2015-07-03 12:53:40 +0000

    ODA-381 Course: Updated documentation, modern noise model for pollute model

--- course/create_for_student.sh
+++ course/create_for_student.sh
@@ -19,32 +19,32 @@ cp exercise_2/load_obs.m        $STUDENT/exercise_2
 
 # Exercise 3
 cp -r exercise_3 $STUDENT/exercise_3
-# Remove solution files and replace them with the dummy files
-rm -f $STUDENT/exercise_3/observations.cvs
-mv $STUDENT/exercise_3/observations_wrong.csv $STUDENT/exercise_3/observations.csv
-rm -f $STUDENT/exercise_3/simple_advection_model/java/src/org/openda/AdvectionStochModelInstance.java
-mv $STUDENT/exercise_3/simple_advection_model/java/src/org/openda/AdvectionStochModelInstance_empty_java \
-   $STUDENT/exercise_3/simple_advection_model/java/src/org/openda/AdvectionStochModelInstance.java
+# Remove stuff that they have to make themselves
+rm $STUDENT/exercise_3/obs1.csv
+rm $STUDENT/exercise_3/obs2.csv
+rm $STUDENT/exercise_3/obs3.csv
+rm $STUDENT/exercise_3/obs1_3.csv
+cp $STUDENT/exercise_3/observations_null.csv $STUDENT/exercise_3/observations.csv
+rm $STUDENT/exercise_3/observations_null.csv
+# rm $STUDENT/exercise_4/simple_wave_model/bin/simple_wave_model.jar
 
 # Exercise 4
 cp -r exercise_4 $STUDENT/exercise_4
-# Remove stuff that they have to make themselves
-rm $STUDENT/exercise_4/obs1.csv
-rm $STUDENT/exercise_4/obs2.csv
-rm $STUDENT/exercise_4/obs3.csv
-rm $STUDENT/exercise_4/obs1_3.csv
-cp $STUDENT/exercise_4/observations_null.csv $STUDENT/exercise_4/observations.csv
-rm $STUDENT/exercise_4/observations_null.csv
-# rm $STUDENT/exercise_4/simple_wave_model/bin/simple_wave_model.jar
 
 # Exercise 5
 cp -r exercise_5 $STUDENT/exercise_5
+rm $STUDENT/exercise_5/EnKF2.oda
+rm $STUDENT/exercise_5/stochObserver/noosObservations2.xml
+rm $STUDENT/exercise_5/stochModel/polluteStochModel2.xml
 
 # Exercise 6
 cp -r exercise_6 $STUDENT/exercise_6
-rm $STUDENT/exercise_6/EnKF2.oda
-rm $STUDENT/exercise_6/stochObserver/noosObservations2.xml
-rm $STUDENT/exercise_6/stochModel/polluteStochModel2.xml
+# Remove solution files and replace them with the dummy files
+rm -f $STUDENT/exercise_6/observations.cvs
+mv $STUDENT/exercise_6/observations_wrong.csv $STUDENT/exercise_6/observations.csv
+rm -f $STUDENT/exercise_6/simple_advection_model/java/src/org/openda/AdvectionStochModelInstance.java
+mv $STUDENT/exercise_6/simple_advection_model/java/src/org/openda/AdvectionStochModelInstance_empty_java \
+   $STUDENT/exercise_6/simple_advection_model/java/src/org/openda/AdvectionStochModelInstance.java
 
 
 # Remove svn stuff
