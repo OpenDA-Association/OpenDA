579fd129c80453248e721d47da21988f771390f1
Martin Verlaan <Martin.Verlaan@deltares.nl>
2015-07-07 13:36:42 +0000

    Issue: ODA-389 Add PythonResultWriter similar to MatlabResultWriter

---
+++ core/java/src/org/openda/resultwriters/PythonResultWriter.java
@@ -0,0 +1,133 @@
+/* MOD_V2.0
+* Copyright (c) 2012 OpenDA Association
+* All rights reserved.
+* 
+* This file is part of OpenDA. 
+* 
+* OpenDA is free software: you can redistribute it and/or modify 
+* it under the terms of the GNU Lesser General Public License as 
+* published by the Free Software Foundation, either version 3 of 
+* the License, or (at your option) any later version. 
+* 
+* OpenDA is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+* GNU Lesser General Public License for more details. 
+* 
+* You should have received a copy of the GNU Lesser General Public License
+* along with OpenDA.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.openda.resultwriters;
+
+import org.openda.interfaces.IInstance;
+import org.openda.interfaces.IResultWriter;
+import org.openda.interfaces.ITreeVector;
+import org.openda.interfaces.IVector;
+import org.openda.utils.Instance;
+import org.openda.utils.Matrix;
+import org.openda.utils.TreeVector;
+import org.openda.utils.Vector;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.HashMap;
+
+/**
+ * Result writer that produces output in a python script file.
+ */
+public class PythonResultWriter implements IResultWriter {
+
+    private static final String commentPrefix = "# ";
+    private PrintStream outputStream = null;
+    private HashMap<String, Integer> iter = new HashMap<String, Integer>();
+    private int defaultMaxSize = 1000;
+
+    public PythonResultWriter(File workingDir, String configString) {
+        if ( configString.startsWith("<xml") ) {  // TODO: right prefix
+            // TODO: read from config file
+        } else {
+            if ( configString.toLowerCase().endsWith(".py") ) {
+                // configString directly indicates the python output file
+                try {
+                    outputStream = new PrintStream(new File(workingDir, configString));
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException("PythonResultWriter: could not open " +
+                            " for writing (" + e.getMessage() + ")");
+                }
+            }
+        }
+        outputStream.println("import numpy as np");
+    }
+
+    public void putMessage(Source source, String comment) {
+        comment = comment.replaceAll("\n", "\n"+commentPrefix);
+        outputStream.println(commentPrefix + comment);
+    }
+
+    public void putMessage(IInstance source, String comment) {
+        comment = comment.replaceAll("\n", "\n"+commentPrefix);
+        outputStream.println(commentPrefix + " " + Instance.identifySource(source) + " " +  comment);
+    }
+
+	public void putValue(Source source, String id, Object result, OutputLevel outputLevel, String context, int iteration) {
+		outputStream.println(commentPrefix + " resultItem id: "+ id +", outputLevel: "+ outputLevel.toString() +", context: "+ context);
+        String prefix = commentPrefix;
+    	// TODO create a counter for time being; one counter per id
+    	Integer currentIter =0;
+    	if(this.iter.containsKey(id)){
+    		currentIter = this.iter.get(id);
+    	}else{
+    		outputStream.println(id+"=[]");
+    	}
+    	this.iter.put(id, currentIter+1);
+        if (result instanceof ITreeVector) {
+            outputStream.print(prefix + " " + ((ITreeVector)result).getId() + ": ");
+            boolean printComma = false;
+            for (String subTreeVectorId : ((ITreeVector)result).getSubTreeVectorIds()) {
+                if (printComma) {
+                    outputStream.print(", ");
+                } else {
+                    printComma = true;
+                }
+                outputStream.print(subTreeVectorId);
+            }
+            outputStream.println();
+        }
+        //MVL outputStream.print(id + (iteration>-1 ? "{"+(iteration+1)+"}" : "") + "=");
+        outputStream.print(id + ".append(");
+        if (result instanceof Matrix) {
+            ((Matrix)result).serialize(outputStream);
+        } else if (result instanceof Vector) {
+            ((Vector)result).serialize(outputStream);
+        } else if (result instanceof TreeVector) {
+            ((TreeVector)result).serialize(outputStream);
+        } else {
+            outputStream.print(result.toString());
+        }
+        outputStream.println(")");
+	}
+
+    public void putIterationReport(IInstance source, int iteration, double cost, IVector parameters) {
+    }
+
+    
+    public int getDefaultMaxSize() {
+        return defaultMaxSize;
+    }
+    
+    public void free(){
+    	outputStream.println(commentPrefix+"Try to merge lists into arrays");
+    	
+    	for( String key : this.iter.keySet()){
+    		outputStream.println("try :");
+    		outputStream.println("   "+key+"=np.vstack("+key+")");
+    		outputStream.println("except :");
+    		outputStream.println("   print(\"Could not merge list into array for "+key+"\")");
+    	}
+		outputStream.println("");
+    };
+
+}
+
---
+++ core/java/test/org/openda/resultwriters/PythonResultWriterTest.java
@@ -0,0 +1,269 @@
+/* MOD_V2.0
+* Copyright (c) 2012 OpenDA Association
+* All rights reserved.
+* 
+* This file is part of OpenDA. 
+* 
+* OpenDA is free software: you can redistribute it and/or modify 
+* it under the terms of the GNU Lesser General Public License as 
+* published by the Free Software Foundation, either version 3 of 
+* the License, or (at your option) any later version. 
+* 
+* OpenDA is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+* GNU Lesser General Public License for more details. 
+* 
+* You should have received a copy of the GNU Lesser General Public License
+* along with OpenDA.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+package org.openda.resultwriters;
+
+import junit.framework.TestCase;
+import org.openda.core.io.castorgenerated.ResultSelectionXML;
+import org.openda.interfaces.*;
+import org.openda.utils.*;
+import org.openda.utils.io.CastorUtils;
+import org.openda.utils.io.OpenDaConfigurationReader;
+import org.openda.utils.TreeVector;
+import org.openda.utils.Vector;
+
+import java.io.File;
+import java.io.IOException;
+
+
+/**
+ * Tests for the configuration of resultWriters
+ */
+public class PythonResultWriterTest extends TestCase {
+
+    //File testDir = null;
+    private File testRunDataDir;
+
+    protected void setUp() throws IOException {
+        OpenDaTestSupport testData = new OpenDaTestSupport(PythonResultWriterTest.class, "core");
+        testRunDataDir = testData.getTestRunDataDir();
+        Results.reset();
+    }
+    
+    //private void setTestDir() {
+    //    testDir = OpenDaTestSupport.getUnitTestDir("openda/test/org/openda/resultwriters");
+    //}
+
+    private void doAlgorithmSteps() {
+        final int maxStep = 3;
+        final int maxOuterIter = 2;
+        final int maxInnerIter = 2;
+        Results.putMessage("starting test");
+
+        IVector v = new Vector(3);
+        v.setConstant(0);
+        
+        ITreeVector v2_part1 = new TreeVector("first_part", new Vector("[1,2,3]"));
+        ITreeVector v2_part2 = new TreeVector("second_part", new Vector("[4,5,6]"));
+        TreeVector v2 = new TreeVector("all");
+        v2.addChild(v2_part1);
+        v2.addChild(v2_part2);
+        Results.putValue("treevector", v2, v2.getSize(), "any", IResultWriter.OutputLevel.Essential, IResultWriter.MessageType.Instance);
+
+
+        Results.putProgression("*** START doAlgorithmSteps ***");
+        for (int i = 0; i < maxStep; i++) {
+            Results.putProgression("Algorithm Step");
+            Results.putProgression(IResultWriter.MessageType.Step);
+            Results.putMessage("Here is an algorithm step");
+            Results.putValue("v1", nextValues(v), nextValues(v).getSize(), "any", IResultWriter.OutputLevel.Essential, IResultWriter.MessageType.Instance);
+            Results.putValue("someValues", nextValues(v), nextValues(v).getSize(), "any", IResultWriter.OutputLevel.Essential, IResultWriter.MessageType.Instance);
+            for (int j = 0; j < maxOuterIter; j++) {
+                Results.putProgression("Outer Iteration");
+                Results.putProgression(IResultWriter.MessageType.OuterIteration);
+                for (int k = 0; k < maxInnerIter; k++) {
+                    Results.putProgression("Inner Iteration");
+                    Results.putProgression(IResultWriter.MessageType.InnerIteration);
+                    Results.putValue("modelValues", nextValues(v), nextValues(v).getSize(), "any", IResultWriter.OutputLevel.Essential, IResultWriter.MessageType.InnerIteration);
+                    Results.putValue("someValues", nextValues(v), nextValues(v).getSize(), "any", IResultWriter.OutputLevel.Essential, IResultWriter.MessageType.InnerIteration);
+                    Results.putValue("costEvaluation", nextValues(v), nextValues(v).getSize(), "any", IResultWriter.OutputLevel.Essential, IResultWriter.MessageType.InnerIteration);
+                }
+                Results.putValue("Outer result", nextValues(v), nextValues(v).getSize(), "any", IResultWriter.OutputLevel.Essential, IResultWriter.MessageType.OuterIteration);
+            }
+            Object dummy = null;
+            Results.putIterationReport((IInstance) dummy,i,nextValues(v).getValue(i),v);
+        }
+    }
+
+    public void testCalibrationCostFunctionResults() {
+
+        //setTestDir();
+        IResultWriter pythonWriter = new PythonResultWriter(testRunDataDir, "python-costs-out.py");
+        Results.addResultWriter(pythonWriter, new ResultSelectionConfig("costEvaluation"));
+        doAlgorithmSteps();
+        Results.reset();
+    }
+
+    public void testCalibrationTreeVectorOutput() {
+
+        //setTestDir();
+        IResultWriter pythonWriter = new PythonResultWriter(testRunDataDir, "python-treevector-out.py");
+        Results.addResultWriter(pythonWriter, new ResultSelectionConfig("treevector"));
+        doAlgorithmSteps();
+        Results.reset();
+    }
+
+    
+    public void testPythonResultsAlgorithmUnknown() {
+
+        //setTestDir();
+        IResultWriter pythonWriter = new PythonResultWriter(testRunDataDir, "python-out.py");
+
+        Results.addResultWriter(pythonWriter);
+        doAlgorithmSteps();
+        Results.reset();
+    }
+
+    public void testPythonResultsFiltering() {
+
+        //setTestDir();
+        IResultWriter pythonWriter = new PythonResultWriter(testRunDataDir, "python-filtering-out.py");
+
+        Results.addResultWriter(pythonWriter);
+        Results.setAlgorithmType(IAlgorithm.Type.Filtering);
+        doAlgorithmSteps();
+        Results.reset();
+    }
+
+    public void testPythonResultsFilteringWithSelection_1() {
+
+        //setTestDir();
+        //ResultWriter pythonWriter = new PythonResultWriter(testRunDataDir, "python-filtering-select-1-out.py");
+        //ResultSelectionConfig selectionConfig = parseSelectionConfig(new File(testRunDataDir, "result_selection_1.xml"));
+        IResultWriter pythonWriter = new PythonResultWriter(testRunDataDir, "python-filtering-select-1-out.py");
+        ResultSelectionConfig selectionConfig = parseSelectionConfig(new File(testRunDataDir, "result_selection_1.xml"));
+
+        Results.addResultWriter(pythonWriter, selectionConfig);
+        Results.setAlgorithmType(IAlgorithm.Type.Filtering);
+        doAlgorithmSteps();
+        Results.reset();
+    }
+
+    public void testPythonResultsFilteringWithSelection_2() {
+
+        //setTestDir();
+        IResultWriter pythonWriter = new PythonResultWriter(testRunDataDir, "python-filtering-select-2-out.py");
+        //ResultSelectionConfig selectionConfig = parseSelectionConfig(new File(testRunDir, "result_selection_2.xml"));
+        ResultSelectionConfig selectionConfig = parseSelectionConfig(new File(testRunDataDir, "result_selection_2.xml"));
+
+        Results.addResultWriter(pythonWriter, selectionConfig);
+        Results.setAlgorithmType(IAlgorithm.Type.Filtering);
+        doAlgorithmSteps();
+        Results.reset();
+    }
+
+    public void testPythonResultsCalibration() {
+
+        //setTestDir();
+        IResultWriter pythonWriter = new PythonResultWriter(testRunDataDir, "python-calibration-out.py");
+
+        Results.addResultWriter(pythonWriter);
+        Results.setAlgorithmType(IAlgorithm.Type.Calibration);
+        doAlgorithmSteps();
+        Results.reset();
+    }
+
+    public void testPythonResultsCalibrationWithSelection_1() {
+
+        //setTestDir();
+        IResultWriter pythonWriter = new PythonResultWriter(testRunDataDir, "python-calibration-select-1-out.py");
+        ResultSelectionConfig selectionConfig = parseSelectionConfig(new File(testRunDataDir, "result_selection_1.xml"));
+
+        Results.addResultWriter(pythonWriter, selectionConfig);
+        Results.setAlgorithmType(IAlgorithm.Type.Calibration);
+        doAlgorithmSteps();
+        Results.reset();
+    }
+
+    public void testPythonResultsCalibrationWithSelection_2() {
+
+        //setTestDir();
+        IResultWriter pythonWriter = new PythonResultWriter(testRunDataDir, "python-calibration-select-2-out.py");
+        ResultSelectionConfig selectionConfig = parseSelectionConfig(new File(testRunDataDir, "result_selection_2.xml"));
+
+        Results.addResultWriter(pythonWriter, selectionConfig);
+        Results.setAlgorithmType(IAlgorithm.Type.Calibration);
+        doAlgorithmSteps();
+        Results.reset();
+    }
+
+
+    private void doAlgorithmMcSteps() {
+        final int maxStep = 3;
+        final int maxOuterIter = 2;
+        final int maxInnerIter = 2;
+        Results.putMessage("starting test");
+
+        IVector v = new Vector(3);
+        v.setConstant(0);
+
+        ITreeVector params = new TreeVector("model_parameters",new String[]{"param1","param2"},new double[]{0.02,0.03});
+        ITreeVector residual1 = new TreeVector("residual1", new Vector("[1,2,3]"));
+        ITreeVector residual2 = new TreeVector("residual2", new Vector("[4,5,6]"));
+        TreeVector v2 = new TreeVector("allResidual");
+        v2.addChild(residual1);
+        v2.addChild(residual2);
+
+        Results.putProgression("*** START doAlgorithmSteps ***");
+        for (int i = 0; i < maxStep; i++) {
+            params.scale((double)(i+1));
+            v2.scale((double) (i+1)*0.6);
+            Results.putValue("evaluatedParameters", params, params.getSize(), "any", IResultWriter.OutputLevel.Essential, IResultWriter.MessageType.Instance);
+            Results.putValue("residuals", v2, v2.getSize(), "any", IResultWriter.OutputLevel.Verbose, IResultWriter.MessageType.Instance);
+        }
+    }
+
+    private ResultSelectionConfig parseSelectionConfig(File selectionFile) {
+        if (!selectionFile.exists()) {
+            throw new RuntimeException("Selection file " + selectionFile.getAbsolutePath() + "does not exist");
+        }
+        ResultSelectionXML resultSelectionXML = (ResultSelectionXML) CastorUtils.parse(selectionFile, ResultSelectionXML.class);
+        return OpenDaConfigurationReader.parseResultSelectionConfig(resultSelectionXML);
+    }
+
+    private IVector nextValues(IVector prevVector) {
+        prevVector.setConstant(prevVector.getValue(0)+1);
+        return prevVector;
+    }
+
+    private void doAlgorithmStepsLevels() {
+        final int maxStep = 3;
+        final int maxOuterIter = 2;
+        final int maxInnerIter = 2;
+        TestInstance testRunner = new TestInstance(null);
+        TestInstance testAlgorithm = new TestInstance(testRunner);
+        Results.putMessage(testRunner, "starting test");
+
+        IVector v = new Vector(3);
+        v.setConstant(0);
+
+        for (int i = 0; i < maxStep; i++) {
+            Results.putProgression(testAlgorithm, "Algorithm Step");
+            Results.putProgression(IResultWriter.MessageType.Step);
+            Results.putMessage(testAlgorithm, "Here is an algorithm step");
+            Results.putValue("someValues", nextValues(v), nextValues(v).getSize(), "any", IResultWriter.OutputLevel.Essential, IResultWriter.MessageType.Instance);
+            TestInstance testOuterLoop = new TestInstance(testAlgorithm);
+            for (int j = 0; j < maxOuterIter; j++) {
+                Results.putProgression(testOuterLoop, "Outer Iteration");
+                TestInstance testInnerLoop = new TestInstance(testOuterLoop);
+                for (int k = 0; k < maxInnerIter; k++) {
+                    Results.putProgression(testInnerLoop, "Inner Iteration");
+                    Results.putValue("modelValues", nextValues(v), nextValues(v).getSize(), "any", IResultWriter.OutputLevel.Essential, IResultWriter.MessageType.InnerIteration);
+                    Results.putValue("someValues", nextValues(v), nextValues(v).getSize(), "any", IResultWriter.OutputLevel.Essential, IResultWriter.MessageType.InnerIteration);
+                    Results.putValue("costEvaluation", nextValues(v), nextValues(v).getSize(), "any", IResultWriter.OutputLevel.Essential, IResultWriter.MessageType.InnerIteration);
+                }
+                Results.putValue("Outer result", nextValues(v), nextValues(v).getSize(), "any", IResultWriter.OutputLevel.Essential, IResultWriter.MessageType.OuterIteration);
+            }
+        }
+    }
+
+
+}
