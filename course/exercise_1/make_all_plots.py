21c21e80842568d8f0a9daf6885a2771b743be58
Martin Verlaan <Martin.Verlaan@deltares.nl>
2015-07-10 11:32:31 +0000

    Issue: ODA-389 Add script with plotting commands for exercise 1

---
+++ course/exercise_1/make_all_plots.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  3 16:38:17 2015
+
+@author: verlaanm
+"""
+
+# ex1 
+
+#load numpy and matplotlib if needed
+import numpy as np
+import matplotlib.pyplot as plt
+
+#load data
+import simulation_unperturbed_results as sim
+
+# make 3d line plot
+from mpl_toolkits.mplot3d import Axes3D
+fig1 = plt.figure()
+ax = fig1.add_subplot(111, projection='3d')
+Axes3D.plot(ax,sim.x[:,0],sim.x[:,1],sim.x[:,2])
+
+#plot of variable 1 (that is o here)
+fig2 = plt.figure()
+plt.plot(sim.model_time,sim.x[:,0])
+plt.plot(sim.analysis_time,sim.obs,'r*')
+
+#load unperturbed and perturbed results
+import simulation_unperturbed_results as sim
+import simulation_perturbed_results as simp
+fig3 = plt.figure()
+ax = fig3.add_subplot(111, projection='3d')
+Axes3D.plot(ax,sim.x[:,0],sim.x[:,1],sim.x[:,2],'b')
+Axes3D.plot(ax,simp.x[:,0],simp.x[:,1],simp.x[:,2],'r')
+
+fig4 = plt.figure()
+plt.plot(sim.model_time,sim.x[:,0])
+plt.plot(simp.model_time,simp.x[:,0],'r')
+
+#load ensemble and plot
+#import simulation_ensemble_results as ens
+#plot(ens.analysis_time,np.transpose([ens.xi_f_0[:,0],ens.xi_f_1[:,0],ens.xi_f_2[:,0],ens.xi_f_3[:,0]]))
