5e4c335437364eba14ea094745fe6f0ce5aa1c67
Stef Hummel <Stef.Hummel@deltares.nl>
2016-04-21 13:31:25 +0000

    Issue: ODA-432 Added method to convert double to bytes (work in progress)
    Former-commit-id: 3bab7ed797453097d78d15cda51300fd3a9f9a46

--- model_delft3d/java/src/org/openda/model_delft3d/D3DBinRestartFile.java
+++ model_delft3d/java/src/org/openda/model_delft3d/D3DBinRestartFile.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
+import java.nio.ByteBuffer;
+
 /**
  * Created by hummel on 20-Apr-16.
  */
@@ -32,8 +34,17 @@ public class D3DBinRestartFile {
 			                   // V1: mMax*nMax + mMax*nMax*nLay (size of S1 plus size of U1) [+ 2 * record divider]
 							   // R1: mMax*nMax + 2*mMax*nMax*nLay (size of S1 plus twice size of U1/V1) [+ 3 * record divider]
 			randomAccessFile.seek(position);
-			byte[] bytes = new byte[0]; // TODO convert from Doubles
-			randomAccessFile.write(bytes);
+			int totalSize = 0; // TODO: mMax*nMax or mMax*nMax*nLay
+			byte[] allBytes = new byte[totalSize];
+			int index = 0;
+			// TODO: loop over all doubles values. in that loop:
+			double value = 0d;
+			byte[] doubleAsBytes = toByteArray(value);
+			for (int i = 0; i < 8; i++) {
+				allBytes[index]=doubleAsBytes[i];
+				index++;
+			}
+			randomAccessFile.write(allBytes);
 		} catch (IOException e) {
 			throw new RuntimeException("Could not jump through binary restart file " + binRestartFilePath.getAbsolutePath());
 		}
@@ -45,5 +56,15 @@ public class D3DBinRestartFile {
 		} catch (IOException e) {
 			throw new RuntimeException("Could not close binary restart file " + binRestartFilePath.getAbsolutePath());
 		}
+	}
+
+	private static byte[] toByteArray(double value) {
+		byte[] bytes = new byte[8];
+		ByteBuffer.wrap(bytes).putDouble(value);
+		return bytes;
+	}
+
+	private static double toDouble(byte[] bytes) {
+		return ByteBuffer.wrap(bytes).getDouble();
 	}
 }
