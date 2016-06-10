40c2e2d7e3c9c396bd45114ebca83e63a4f9b6e1
Erik Pelgrim <Erik.Pelgrim@deltares.nl>
2016-06-10 08:14:56 +0000

    ODA-460 Use Locale.US in DataObjects: Sobek3XyzRestartFile and D3dMdFileDataObject
    Former-commit-id: b256a261d021927aba6094a60197e57407b2a061

--- model_delft3d/java/src/org/openda/model_delft3d/D3dMdFileDataObject.java
+++ model_delft3d/java/src/org/openda/model_delft3d/D3dMdFileDataObject.java
@@ -10,8 +10,6 @@ import org.openda.interfaces.IPrevExchangeItem;
 import java.io.*;
 import java.util.*;
 
-import static org.openda.model_delft3d.ModelDefinitionFile.fileKeys;
-
 /**
  * Created by Theo on 12.04.2016.
  */
@@ -151,7 +149,7 @@ public class D3dMdFileDataObject implements IDataObject {
 								valueAsdouble = getDiffFromMjd(valueAsdouble);
 							}
 
-							String valueAsString = String.format("%.7e",valueAsdouble);
+							String valueAsString = String.format(Locale.US, "%.7e",valueAsdouble);
 							line = exchangeItemIDs[i] + " = " + valueAsString;
 						}
 					}
