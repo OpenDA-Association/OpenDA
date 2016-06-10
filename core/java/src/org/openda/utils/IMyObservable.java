1fb3ed7d395de47aba4267841289e82f29e9927e
Stef Hummel <Stef.Hummel@deltares.nl>
2016-06-10 12:45:00 +0000

    Merged revision(s) 516 from branches/openda_2_3: Added version/license headers to files that did not have them yet (note: this should have been done before creating the branch)
    Former-commit-id: 96577b64832d91d360201ae58220a80a1f1e6d03

--- core/java/src/org/openda/utils/IMyObservable.java
+++ core/java/src/org/openda/utils/IMyObservable.java
@@ -1,19 +1,23 @@
+/* MOD_V2.0 
+* Copyright (c) 2012 OpenDA Association 
+* All rights reserved.
+* 
+* This file is part of OpenDA. 
+* 
+* OpenDA is free software: you can redistribute it and/or modify 
+* it under the terms of the GNU Lesser General Public License as 
+* published by the Free Software Foundation, either version 3 of 
+* the License, or (at your option) any later version. 
+* 
+* OpenDA is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+* GNU Lesser General Public License for more details. 
+* 
+* You should have received a copy of the GNU Lesser General Public License
+* along with OpenDA.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.openda.utils;
-
-/**
- * Subset of java.util.Observable, Observer methods. These classes in java annoyingly do not implement 
- * interfaces, so they can not be used when a class must extend another class, because java
- * does not support multiple inheritance
- * 
- * The two interfaces IMyObservable and IMyObserver together implement the observer-pattern.
- * Standard implementations MyObservable and MyObserver can be used to simplify implementation.
- * 
- * Intro: The observer pattern implements a call-bck mechanism to notify other classes when something 
- * has happened. This avoids frequent polling (asking if something has happened) and is more modular. 
- * 
- * @author verlaanm
- *
- */
 public interface IMyObservable {
 
 	/**
--- model_delft3d/java/src/org/openda/model_delft3d/dll/D3dFlowWinIfortDll.java
+++ model_delft3d/java/src/org/openda/model_delft3d/dll/D3dFlowWinIfortDll.java
@@ -1,33 +1,23 @@
-/*----- LGPL --------------------------------------------------------------------
- *
- *  Copyright (C)  Stichting Deltares, 2011.
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation version 2.1.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, see <http://www.gnu.org/licenses/>.
- *
- *  contact: delft3d.support@deltares.nl
- *  Stichting Deltares
- *  P.O. Box 177
- *  2600 MH Delft, The Netherlands
- *
- *  All indications and logos of, and references to, "Delft3D" and "Deltares"
- *  are registered trademarks of Stichting Deltares, and remain the property of
- *  Stichting Deltares. All rights reserved.
- *
- *-------------------------------------------------------------------------------
- *  $Id:  $
- *  $HeadURL:  $
- */package org.openda.model_delft3d.dll;
-
+/* MOD_V2.0 
+* Copyright (c) 2012 OpenDA Association 
+* All rights reserved.
+* 
+* This file is part of OpenDA. 
+* 
+* OpenDA is free software: you can redistribute it and/or modify 
+* it under the terms of the GNU Lesser General Public License as 
+* published by the Free Software Foundation, either version 3 of 
+* the License, or (at your option) any later version. 
+* 
+* OpenDA is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+* GNU Lesser General Public License for more details. 
+* 
+* You should have received a copy of the GNU Lesser General Public License
+* along with OpenDA.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openda.model_delft3d.dll;
 import com.sun.jna.Library;
 import com.sun.jna.ptr.DoubleByReference;
 import com.sun.jna.ptr.IntByReference;
