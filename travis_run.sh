297b6eb9a15f325f1121e486fb02a60eaf25b8c0
Nils van Velzen <nilsvanvelzen@gmail.com>
2016-09-26 12:56:35 +0000

    added install script for the native stuff and installing all packages
    Former-commit-id: c0cea54ec7a49767b493150c30ea9a155a7a828f

---
+++ travis_run.sh
@@ -0,0 +1,12 @@
+sudo apt-get install gfortran
+sudo apt-get install openmpi-dev
+sudo apt-get install libnetcdf-dev
+sudo apt-get install liblapack-dev
+HIER=$PWD
+cd core/native
+./autoreconf_fix.sh
+./configure
+make install
+cd $HIER
+export OPENDADIR=$PWD/bin
+export LD_LYBRARY_PATH=$LD_LIBRARY_PATH:$OPENDADIR/bin/linux64_gnu/lib
