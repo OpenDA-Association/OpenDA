e589f28010a7ad65320c7b4c356ea90f98cfc46a
Erik Pelgrim <Erik.Pelgrim@deltares.nl>
2016-11-11 08:41:18 +0000

    ODA-508 Offer 1:n NoiseState/ModelState ratio to fasten noise model initialisation, add DummyStateObject
    Former-commit-id: c6307c1a809c961d788b578e04994eb92b523a77

---
+++ core/java/test/org/openda/blackbox/wrapper/DummyStateDataObject.java
@@ -0,0 +1,65 @@
+package org.openda.blackbox.wrapper;
+
+import org.openda.exchange.DoubleExchangeItem;
+import org.openda.exchange.DoublesExchangeItem;
+import org.openda.interfaces.IDataObject;
+import org.openda.interfaces.IExchangeItem;
+import org.openda.interfaces.IPrevExchangeItem;
+
+import java.io.File;
+
+/**
+ * Created by pelgrim on 28-Oct-16.
+ */
+public class DummyStateDataObject implements IDataObject {
+    private static int stateSize;
+
+    @Override
+    public String[] getExchangeItemIDs() {
+        return new String[]{"state", "StartTime", "StopTime", "TimeStep"};
+    }
+
+    @Override
+    public String[] getExchangeItemIDs(IPrevExchangeItem.Role role) {
+        throw new RuntimeException("org.openda.blackbox.wrapper.DummyStateDataObject.getExchangeItemIDs() not implemented yet");
+
+    }
+
+    @Override
+    public IExchangeItem getDataObjectExchangeItem(String exchangeItemID) {
+        switch (exchangeItemID) {
+            case "state" :
+                //double[] values = new double[1061];
+                double[] values = new double[stateSize];
+                fillWithRandomValues(values);
+                return new DoublesExchangeItem("state", IPrevExchangeItem.Role.InOut, values);
+            case "StartTime" :
+                return new DoubleExchangeItem("StartTime", IPrevExchangeItem.Role.InOut, 0);
+            case "StopTime" :
+                return new DoubleExchangeItem("StopTime", IPrevExchangeItem.Role.InOut, 10);
+            case "TimeStep" :
+                return new DoubleExchangeItem("TimeStep", IPrevExchangeItem.Role.InOut, 1);
+            default:
+                return null;
+        }
+    }
+
+    private void fillWithRandomValues(double[] values) {
+        for (int i = 0; i < values.length; i++) {
+			values[i] = Math.random();
+		}
+    }
+
+    @Override
+    public void finish() {
+    }
+
+    @Override
+    public void initialize(File workingDir, String[] arguments) {
+
+    }
+
+    public static void setStateSize(int size) {
+        stateSize = size;
+    }
+}
