93564a6e4550b0195167843fde242b725ebed404
Werner Kramer <werner.kramer@vortech.nl>
2017-05-05 08:45:39 +0000

    Merged revision(s) 777 from branches/openda_2_3: ODA-552: Fix call AnnounceObservedValues in CtaOpenDaModel when no observations are present. ........
    Former-commit-id: d1379275d07567b3a547245115b01795fd855495

--- core/java/src/org/openda/costa/CtaOpenDaModel.java
+++ core/java/src/org/openda/costa/CtaOpenDaModel.java
@@ -87,7 +87,7 @@ public class CtaOpenDaModel extends CtaObject implements IStochModelInstance, IS
 			ctaAnnounceObservedValues(observationDescriptions);
 		}
 		else {
-			if(observationDescriptions.getTimes().length>1){
+			if(observationDescriptions.getTimes()!= null && observationDescriptions.getTimes().length>1){
 				timerAnnounce.stop();
 				throw new UnsupportedOperationException("org.costa.CtaOpenDaModel.announceObservedValues not implemented yet for other types");
 			}
