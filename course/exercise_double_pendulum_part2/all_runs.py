167d50fc226ecc141f6fde436fed48005d1d20d5
Martin Verlaan <Martin.Verlaan@deltares.nl>
2017-07-15 18:16:43 +0000

    Issue: ODA-570 Update of plotting code after x_a_central vs x_f_central bugfix
    Former-commit-id: d0e570c52d62f7289d59ed3ada3e7c5b9613422d

--- course/exercise_double_pendulum_part2/all_runs.py
+++ course/exercise_double_pendulum_part2/all_runs.py
@@ -9,75 +9,95 @@ Script that will produce the figures of exercise 2
 #load numpy and matplotlib if needed
 import numpy as np
 import matplotlib.pyplot as plt
-import ensemble
 
-# laod data fomr enkf_results_estd1_ens10, collect ensemble in one 3d-array 
-# and compute ensemble mean of first [0] element in statevector (state=[x,y,z]) 
-import enkf_results_std1_ens10 as std1
-(t,enkf)=ensemble.reshape_ensemble(std1)
-enkf1=enkf[:,0,:] #note we start counting at 0
 
-#
-#plot all ensemble members
-#
-fig1 = plt.figure()
-plt.plot(t,enkf1)
-plt.title("Lorenz model first variable (EnKF)")
-plt.savefig("figure_1.png")
+import simulation_truth_results as truth
+import simulation_initial_results as initial
+import simulation_enkf_results as enkf
 
-#
-#plot ensemble mean and observations
-#
-fig2 = plt.figure()
-plt.plot(t,np.mean(enkf1,1))
-plt.plot(t,std1.obs[:,0],"r*")
-plt.title("Lorenz model mean and observations of first variable (EnKF)")
-plt.legend(("ensemble mean for x, std=1","observations"))
-plt.savefig("figure_2.png")
+plt.figure()
+plt.clf()
+plt.subplot(2,1,1)
+plt.plot(initial.model_time,initial.x[:,0],'g')
+plt.plot(truth.model_time,truth.x[:,0],'k')
+plt.plot(enkf.analysis_time,enkf.x_f_central[:,0],'b');
+plt.legend(('initial','truth','EnKF'))
+plt.ylabel(r'$\theta_1$')
+plt.subplot(2,1,2)
+plt.plot(initial.model_time,initial.x[:,1],'g')
+plt.plot(truth.model_time,truth.x[:,1],'k')
+plt.plot(enkf.analysis_time,enkf.x_f_central[:,1],'b');
+plt.ylabel(r'$\theta_2$')
+plt.xlabel(r'$t$')
+plt.savefig('fig_series_enkf.png')
+plt.show() 
 
-#
-#now for std=10
-#
-import enkf_results_std10_ens10 as std10
-(t,enkf)=ensemble.reshape_ensemble(std10)
-enkf10=enkf[:,0,:] #note we start counting at 0
 
-#
-#plot all ensemble members
-#
-fig3 = plt.figure()
-plt.plot(t,enkf10)
-plt.title("Lorenz model first variable (EnKF)")
-plt.savefig("figure_3.png")
+import simulation_enkf_results_seed31415 as enkf2
 
-#
-#plot ensemble mean and observations
-#
-fig4 = plt.figure()
-plt.plot(t,np.mean(enkf10,1))
-plt.plot(t,std10.obs[:,0],"r*")
-plt.title("Lorenz model mean and observations of first variable (EnKF)")
-plt.legend(("ensemble mean for x, std=10","observations"))
-plt.savefig("figure_4.png")
+plt.figure()
+plt.clf()
+plt.subplot(2,1,1)
+plt.plot(initial.model_time,initial.x[:,0],'g')
+plt.plot(truth.model_time,truth.x[:,0],'k')
+plt.plot(enkf.analysis_time,enkf.x_f_central[:,0],'b');
+plt.plot(enkf2.analysis_time,enkf2.x_f_central[:,0],'r');
+plt.legend(('initial','truth','EnKF seed=21','EnKF seed=31415'))
+plt.ylabel(r'$\theta_1$')
+plt.subplot(2,1,2)
+plt.plot(initial.model_time,initial.x[:,1],'g')
+plt.plot(truth.model_time,truth.x[:,1],'k')
+plt.plot(enkf.analysis_time,enkf.x_f_central[:,1],'b');
+plt.plot(enkf2.analysis_time,enkf2.x_f_central[:,1],'r');
+plt.ylabel(r'$\theta_2$')
+plt.xlabel(r'$t$')
+plt.savefig('fig_series_enkf_seed31415.png')
+plt.show() 
 
-#
-# compare data for different ensemble sizes
-#
-import enkf_results_std5_ens5 as ens5
-(t,ens5.ens)=ensemble.reshape_ensemble(ens5)
-ens5.x=np.mean(ens5.ens[:,0,:],1)
-import enkf_results_std5_ens10 as ens10
-(t,ens10.ens)=ensemble.reshape_ensemble(ens10)
-ens10.x=np.mean(ens10.ens[:,0,:],1)
-import enkf_results_std5_ens100 as ens100
-(t,ens100.ens)=ensemble.reshape_ensemble(ens100)
-ens100.x=np.mean(ens100.ens[:,0,:],1)
-#make plot
-fig5 = plt.figure()
-plt.plot(t,ens5.x,'k-')
-plt.plot(t,ens10.x,'b-')
-plt.plot(t,ens100.x,'g-')
-plt.plot(t,ens10.obs[:,0],"r*")
-plt.legend(("n=5","n=10","n=100","observations"))
-plt.savefig("figure_5.png")
+
+import simulation_enkf_results_stdobs2 as enkf2
+
+plt.figure()
+plt.clf()
+plt.subplot(2,1,1)
+plt.plot(initial.model_time,initial.x[:,0],'g')
+plt.plot(truth.model_time,truth.x[:,0],'k')
+plt.plot(enkf.analysis_time,enkf.x_f_central[:,0],'b');
+plt.plot(enkf2.analysis_time,enkf2.x_f_central[:,0],'r');
+plt.legend(('initial','truth','EnKF $\sigma_0=0.2$','EnKF $\sigma_o=2.0$'))
+plt.ylabel(r'$\theta_1$')
+plt.subplot(2,1,2)
+plt.plot(initial.model_time,initial.x[:,1],'g')
+plt.plot(truth.model_time,truth.x[:,1],'k')
+plt.plot(enkf.analysis_time,enkf.x_f_central[:,1],'b');
+plt.plot(enkf2.analysis_time,enkf2.x_f_central[:,1],'r');
+plt.ylabel(r'$\theta_2$')
+plt.xlabel(r'$t$')
+plt.savefig('fig_series_enkf_std2.png')
+plt.show() 
+
+
+import simulation_enkf_results_ens6 as enkf3
+import simulation_enkf_results_ens10 as enkf4
+
+plt.figure()
+plt.clf()
+plt.subplot(2,1,1)
+plt.plot(initial.model_time,initial.x[:,0],'g')
+plt.plot(truth.model_time,truth.x[:,0],'k')
+plt.plot(enkf.analysis_time,enkf.x_f_central[:,0],'b');
+plt.plot(enkf3.analysis_time,enkf3.x_f_central[:,0],'r');
+plt.plot(enkf4.analysis_time,enkf4.x_f_central[:,0],'m');
+plt.legend(('initial','truth','EnKF n=50','EnKF n=6','Enkf n=10'))
+plt.ylabel(r'$\theta_1$')
+plt.subplot(2,1,2)
+plt.plot(initial.model_time,initial.x[:,1],'g')
+plt.plot(truth.model_time,truth.x[:,1],'k')
+plt.plot(enkf.analysis_time,enkf.x_f_central[:,1],'b');
+plt.plot(enkf3.analysis_time,enkf3.x_f_central[:,1],'r');
+plt.plot(enkf4.analysis_time,enkf4.x_f_central[:,1],'m');
+plt.ylabel(r'$\theta_2$')
+plt.xlabel(r'$t$')
+plt.savefig('fig_series_enkf_ens_size.png')
+plt.show() 
 
