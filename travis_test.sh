8043671721d1b48a012f9d535185ad9a7f12da1a
Nils van Velzen <nilsvanvelzen@gmail.com>
2017-10-03 15:02:37 +0000

    ODA-597 Updated travis build
    Former-commit-id: b7252c626b4fbe6b07a72577eb31f80e646e781b

--- travis_test.sh
+++ travis_test.sh
@@ -2,7 +2,16 @@
 echo "Start of script travis_test.sh"
 echo "PWD=$PWD"
 export OPENDADIR=$PWD/bin
-. $OPENDADIR/settings_local.sh linux
+ls -tralala
+echo debug1
+ls -tralal bin
+echo debug2
+source bin/settings_local.sh linux
+echo debug3
+
+
+source $OPENDADIR/settings_local.sh linux
+echo debug4
 env
 
 ant test-travis
