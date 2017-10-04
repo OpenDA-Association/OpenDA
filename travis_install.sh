4a69729de3a61df94863faa22a62c17f73a4348b
Nils van Velzen <nilsvanvelzen@gmail.com>
2017-10-04 11:41:35 +0000

    ODA-597 Updated travis build
    Former-commit-id: 761f564f807983c78e9c988994681a4533821b80

--- travis_install.sh
+++ travis_install.sh
@@ -2,6 +2,10 @@ sudo apt-get install gfortran
 sudo apt-get install mpi-default-dev
 sudo apt-get install libnetcdf-dev
 sudo apt-get install liblapack-dev
+cd ..
+mv OpenDA public
+cd public
+
 HIER=$PWD
 cd core/native
 ./autoreconf_fix.sh
@@ -9,8 +13,6 @@ cd core/native
 make install
 cd $HIER
 
-ant build
+ant build 
 export OPENDADIR=$PWD/bin
 export LD_LYBRARY_PATH=$LD_LIBRARY_PATH:$OPENDADIR/bin/linux64_gnu/lib
-ln -s $PWD ../public
-#ant test-travis
