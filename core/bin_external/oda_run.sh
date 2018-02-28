2ae88f9c9897b9be5e4cc4a2266030b79ea494e6
Alja Vrieling <alja.vrieling@vortech.nl>
2018-02-28 15:45:13 +0000

    ODA-516: optional command line argument for name of logfile -o=<log> (default=openda_logfile.txt)
    Former-commit-id: fae9e56061df76ce7eec3257dde473c6e83da963

--- core/bin_external/oda_run.sh
+++ core/bin_external/oda_run.sh
@@ -1,16 +1,62 @@
 #! /bin/bash
 #
 # Purpose : startup script for openDA applications
-# Args    : -gui   to start the gui
-#           -p     perform a parallel run
-#           <file>  to open configuration file
-# Examples: Application.sh               : starts gui
-#           Application.sh -gui          : starts gui
-#           Application.sh main.xml      : starts computations without gui
-#           Application.sh -gui main.xml : starts gui and opens the configuration file main.xml
-#           The main xml-file now often has the extension .oda
+# Args    : -gui      to start the gui
+#           -p        perform a parallel run
+#           -o=<log>  overrule default name of log file (openda_logfile.txt)
+#           <file>    configuration file
+# Examples: oda_run.sh               : starts gui
+#           oda_run.sh -gui          : starts gui
+#           oda_run.sh main.oda      : starts computations without gui
+#           oda_run.sh -p main.oda   : starts parallel computations without gui
+#           oda_run.sh -gui main.oda : starts gui and opens the configuration file main.oda
+#           oda_run.sh -o=mylog.txt main.oda
+#                                    : start computations without gui, writes log info to mylog.txt.
+#           oda_run.sh -o=mylog.txt -p main.oda
+#                                    : start parallel computation without gui, writes log info to mylog.txt
 # Author  : M. verlaan
 # License : GPL
+
+if [ "$#" -eq 0 ]; then
+    GUI=true
+elif [ "$#" \> 4 ]; then
+    echo "Something wrong: check the input parameters of this script."
+    exit 1;
+else
+    # default settings
+    LOGFILE="openda_logfile.txt"
+    GUI=false
+    PARALLEL=""
+fi
+
+for i in "$@"
+do
+    case $i in
+        -gui)
+        GUI=true
+        shift
+        ;;
+        -o=*|-out=*)
+        LOG=true
+        LOGFILE="${i#*=}"
+        shift
+        ;;
+        -p)
+        PARALLEL="-p"
+        shift
+        ;;
+        *)
+        CONFIG="${i#*=}"
+        configfile="`dirname $CONFIG`/$CONFIG"
+        shift
+        ;;
+    esac
+done
+
+if [ ! -f $configfile ]; then
+    echo "OpenDa config file $configfile not found"
+    exit 1;
+fi
 
 # read local settings
 if [ -z "$OPENDADIR" ];then
@@ -24,7 +70,7 @@ fi
 
 # java options
 if [ -z "$ODA_JAVAOPTS" ]; then
-	export ODA_JAVAOPTS='-Xmx1024m'
+   export ODA_JAVAOPTS='-Xmx1024m'
 fi
 
 # append all jars in opendabindir to java classpath
@@ -35,54 +81,43 @@ for file in $OPENDADIR/*.jar ; do
 done
 
 # starting from bin-directory is necessary for *.so loading now TODO fix this
-if [ $# -eq 0 ] ; then
-   java $ODA_JAVAOPTS org.openda.application.OpenDaApplication -gui
-elif [ $# -eq 1 ]; then
-   if [ "$1" == "-gui" ]; then
-      java $ODA_JAVAOPTS org.openda.application.OpenDaApplication -gui
-   else
-
-      configFile=$1
-      if [ ! -f $configFile ]; then
-         echo "OpenDa config file $configFile not found"
-         exit 1
-      fi
-
-      logfile="`dirname $configFile`/openda_logfile.txt"
-
-      echo "========================================================================="
-      echo Starting "java org.openda.application.OpenDaApplication $1 > $logfile  2>&1"
-
-      # start timing
-      STARTRUN=`date +%s` 
-      echo "$configFile" > $logfile
-      date "+%F, %H:%M:%S" >> $logfile 2>&1
-
-      # run application
-      java $ODA_JAVAOPTS org.openda.application.OpenDaApplication $configFile > $logfile  2>&1
-
-      # end timing
-      date "+%F, %H:%M:%S" >> $logfile 2>&1
-      ENDRUN=`date +%s` 
-      declare -i DURATION
-      DURATION=($ENDRUN-$STARTRUN)
-      echo DURATION, "$DURATION" >> $logfile 2>&1
-
-      echo "Run finished"
-      echo "========================================================================="
-
-   fi
-elif [ $# -eq 2 ]; then
-   if [ "$1" != "-gui" -a  "$1" != "-p" ]; then
-         echo "This scipt accepts no more than one file and option -gui or -p"
-      exit 1
-   fi
-   if [ ! -f $2 ]; then
-         echo "OpenDa config file $2 not found"
-      exit 1
-   fi
-   java org.openda.application.OpenDaApplication $1 $2
+if [ $GUI == true ]; then
+    if [[ ! -z ${PARALLEL} || ${LOG} = true ]]; then
+        echo "Options -p and -o=<log> ignored in combination with -gui."
+    fi
+    if [ $configfile ]; then
+        #echo "start OpenDA with option -gui ${configfile}"
+        java $ODA_JAVAOPTS org.openda.application.OpenDaApplication -gui $configfile
+    else
+        #echo "start OpenDA with option -gui"
+        java $ODA_JAVAOPTS org.openda.application.OpenDaApplication -gui
+    fi
 else
-   echo "This scipt accepts no more than one file and option -gui"
-fi
+    if [ ! $configfile ]; then
+        echo "OpenDA config file missing."
+        exit 1;
+    fi
 
+    logfile="`dirname $CONFIG`/$LOGFILE"
+
+    echo "========================================================================="
+    echo Starting "java org.openda.application.OpenDaApplication ${PARALLEL} > $logfile  2>&1"
+
+    # start timing
+    STARTRUN=`date +%s`
+    echo "$configFile" > $logfile
+    date "+%F, %H:%M:%S" >> $logfile 2>&1
+
+    # run application
+    java $ODA_JAVAOPTS org.openda.application.OpenDaApplication $configFile > $logfile  2>&1
+
+    # end timing
+    date "+%F, %H:%M:%S" >> $logfile 2>&1
+    ENDRUN=`date +%s`
+    declare -i DURATION
+    DURATION=($ENDRUN-$STARTRUN)
+    echo DURATION, "$DURATION" >> $logfile 2>&1
+
+    echo "Run finished"
+    echo "========================================================================="
+fi
