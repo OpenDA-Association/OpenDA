49ebab670ae99c624abe0ae60225cdee61e5402f
Nils van Velzen <nilsvanvelzen@gmail.com>
2018-07-27 15:18:49 +0800

    various improvements to the script and model setup of black box enkf polution exercise

--- course/exercise_black_box_enkf_polution/plot_movie_enkf.py
+++ course/exercise_black_box_enkf_polution/plot_movie_enkf.py
@@ -16,6 +16,7 @@ import reactive_pollution_model_truth as truth
 import sequentialSimulation_results as sim
 #import sequentialEnsembleSimulation_results as ens
 import enkf_results as enkf
+#import enkf_results3 as enkf
 #import enkf_results2 as enkf
 
 # create initial plot
@@ -25,7 +26,10 @@ f,ax = plt.subplots(2,1)
 #offsets for chunks in state vector
 ngrid=len(truth.c1)
 no_sources=len(truth.source_locations)
+n_noise_enkf = len(enkf.x_a[0])-2*ngrid
 
+print("Number of noise parameters in EnKF model run"+str(n_noise_enkf))
+print("no_sources="+str(no_sources))
 # split sources and outputs based on substance
 stypeisone=np.array(truth.source_substance)==1
 stypeistwo=np.array(truth.source_substance)==2
@@ -43,7 +47,7 @@ for i in range(len(enkf.analysis_time)):
    ax[0].plot(truth.c1_map[i],'k')
    c1_sim=sim.x_a[i,(no_sources-1):(no_sources+ngrid)]
    ax[0].plot(c1_sim,'b')
-   c1_enkf=enkf.x_a[i,(no_sources-1):(no_sources+ngrid)]
+   c1_enkf=enkf.x_a[i,(n_noise_enkf):(no_sources+ngrid)]
    ax[0].plot(c1_enkf,'g')
    ax[0].plot(oloc1,0*oloc1+1,'*')
    ax[0].plot(sloc1,0*sloc1+1,'d')
@@ -51,7 +55,7 @@ for i in range(len(enkf.analysis_time)):
    ax[1].plot(truth.c2_map[i],'k')
    c2_sim=sim.x_a[i,(no_sources+ngrid-1):(no_sources+2*ngrid)]
    ax[1].plot(c2_sim,'b')
-   c2_enkf=enkf.x_a[i,(no_sources+ngrid-1):(no_sources+2*ngrid)]
+   c2_enkf=enkf.x_a[i,(n_noise_enkf+ngrid):(n_noise_enkf+2*ngrid)]
    ax[1].plot(c2_enkf,'g')
    ax[1].plot(oloc2,0*oloc2+1,'*')
    ax[1].plot(sloc2,0*sloc2+1,'d')
