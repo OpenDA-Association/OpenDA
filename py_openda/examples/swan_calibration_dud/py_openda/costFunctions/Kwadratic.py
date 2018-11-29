f346df63e6c78358abf05a927427e68c40805828
Rick Hegeman <rick_pc_hegeman@hotmail.com>
2018-11-29 12:03:22 +0100

    Included the utility functions in the py_openda library. Also tidied up the code a bit.

--- py_openda/examples/swan_calibration_dud/py_openda/costFunctions/Kwadratic.py
+++ py_openda/examples/swan_calibration_dud/py_openda/costFunctions/Kwadratic.py
@@ -12,150 +12,95 @@ Created on Wed Oct 10 14:39:10 2018
 import os
 from py4j.java_gateway import JavaGateway
 
+import py_openda.utils.py4j_utils as utils
+
 gateway = JavaGateway()   # connect to the JVM
 
 #os.chdir('/v3/Stage/Rick/openda/openda_public/py_openda/examples/py_swan_calibration')
 scriptdir = os.getcwd()
 #scriptdir = '/v3/Stage/Rick/openda/openda_public/py_openda/examples/py_swan_calibration'
 
-cost_function = None
-model_params = None
-model_ini = None
-observer = None
 
 
 
 
 
 
-def py_list_to_j_array(py_x):
-    """
-    Create a java double array from python list
 
-    :param py_x: python list of doubles
-    :return: java array of doubles copy of py_x
-    """
-    n = len(py_x)
-    double_class = gateway.jvm.double
-    j_x = gateway.new_array(double_class, n)
-
-    for i in range(n):
-        j_x[i] = float(py_x[i])
-
-    return j_x
-
-def j_array_to_py_list(j_x):
-    """
-    Create a python list from a java array
+class Kwadratic:
     
-    :param j_x: java array
-    :return: python list with values of j_x
-    """
-
-    n = len(j_x)
-    py_x = [None]*n
-    for i in range(n):
-        py_x[i] = j_x[i]
-
-    return py_x
-
-
-def initialize_openda_configurable(openda_configurable, input_dir, config_xml):
-    """
-    Initialise an OpenDA configurable (e.g. ModelFactory, StochObserver)
-
-    :param openda_configurable: OpenDA object implementing IConfigurable
-    :param input_dir: Working directory/input directory
-    :param config_xml: Configuration file
-    :return:
-    """
-
-    # Translate the input strings to java objects (File and String array)
-    j_input_dir = gateway.jvm.java.io.File(input_dir)
-    string_class = gateway.jvm.String
-    j_arguments = gateway.new_array(string_class, 1)
-    j_arguments[0] = config_xml
-
-    # Initialize ..
-    openda_configurable.initialize(j_input_dir, j_arguments)
-
-def setup():
-    """
-    Setup the OpenDA model factory, stoch observer and object function
-
-    :return: a tuple containing an instance of the cost function, the initial parameters, an instance of the model and a stoch observer
-    """
-    global cost_function
-    global model_params
-    global model_ini
-    global observer
-
-    #Initialize the model factory
-    #model_input_dir = "/Users/nils/Develop/py4j/swanModel/config"
-    model_input_dir = os.path.join(scriptdir, 'swanModel', 'config')
-    model_config_xml = "swanStochModelConfig.xml"
-
-
-    model_factory = gateway.jvm.org.openda.model_swan.SwanCalibStochModelFactory()
-    initialize_openda_configurable(model_factory, model_input_dir, model_config_xml)
-
-    #Initialize stoch observer
-    #observer_input_dir = "/Users/nils/Develop/py4j/stochObserver"
-    observer_input_dir = model_input_dir = os.path.join(scriptdir, 'stochObserver')
-    observer_config_xml = "swanStochObsConfig.xml"
-
-    observer = gateway.jvm.org.openda.observers.IoObjectStochObserver()
-    initialize_openda_configurable(observer, observer_input_dir, observer_config_xml)
-
-    #Initialize cost function org.openda.algorithms
-    cost_function = gateway.jvm.org.openda.algorithms.SimulationKwadraticCostFunction(model_factory, observer)
-
-    #Get initial parameter
-    outputLevel = gateway.jvm.org.openda.interfaces.IStochModelFactory.OutputLevel.Debug
-    model_ini = model_factory.getInstance(outputLevel)
-    p = model_ini.getParameters()
-    model_params = p
+    def __init__(self):
+        
+        #Initialize the model factory
+        #model_input_dir = "/Users/nils/Develop/py4j/swanModel/config"
+        model_input_dir = os.path.join(scriptdir, 'swanModel', 'config')
+        model_config_xml = "swanStochModelConfig.xml"
     
-    return(cost_function, model_params, model_ini, observer)
-
-def get_obs():
-    """
-    Find the observations and corresponding uncertainties at the relevant locations 
     
-    :return: a tuple containing a list of the mean of all observations at each locations, followed by their respective standard deviations
-    """
-    selectionTimes = model_ini.getTimeHorizon()
-    observerSelection = observer.createSelection(selectionTimes)
-    obs_mean = observerSelection.getExpectations()
-    obs_mean = j_array_to_py_list(obs_mean.getValues())
-    obs_std = observerSelection.getStandardDeviations()
-    obs_std = j_array_to_py_list(obs_std.getValues())
+        model_factory = gateway.jvm.org.openda.model_swan.SwanCalibStochModelFactory()
+        utils.initialize_openda_configurable(model_factory, model_input_dir, model_config_xml)
     
-    return (obs_mean, obs_std)
+        #Initialize stoch observer
+        #observer_input_dir = "/Users/nils/Develop/py4j/stochObserver"
+        observer_input_dir = model_input_dir = os.path.join(scriptdir, 'stochObserver')
+        observer_config_xml = "swanStochObsConfig.xml"
+    
+        observer = gateway.jvm.org.openda.observers.IoObjectStochObserver()
+        utils.initialize_openda_configurable(observer, observer_input_dir, observer_config_xml)
+        
+        
+        #Initialize cost function org.openda.algorithms
+        self.cost_function = gateway.jvm.org.openda.algorithms.SimulationKwadraticCostFunction(model_factory, observer)
+    
+        #Get initial parameter
+        outputLevel = gateway.jvm.org.openda.interfaces.IStochModelFactory.OutputLevel.Debug
+        self.model_instance = model_factory.getInstance(outputLevel)
+        selectionTimes = self.model_instance.getTimeHorizon()
+        self.selection = observer.createSelection(selectionTimes)
+        self.target_time = self.model_instance.getTimeHorizon().getEndTime()
 
-def object_function(p):
-    """
-    Compute the predictions of the object function for parameters p
-    :param p: parameters
-    :return: list of predictions
-    """
-
-    # Create an OpenDA TreeVector with parameter value
-    p_new = model_params.clone()
-    j_p = py_list_to_j_array(p)
-    p_new.setValues(j_p)
-    model_new = model_ini
-    model_new.setParameters(p_new)
-
-    selectionTimes = model_new.getTimeHorizon()
-    observerSelection = observer.createSelection(selectionTimes)
-    targetTime = model_new.getTimeHorizon().getEndTime()
-    model_new.compute(targetTime)
-
-    descr = observerSelection.getObservationDescriptions()
-    prd = model_new.getObservationOperator().getObservedValues(descr)
-    prd = j_array_to_py_list(prd.getValues())
-
-    val = cost_function.evaluate(p_new, "-")
-    print("Cost="+str(val)+" p="+str(p))
-    return prd
+    def get_parameters(self):
+        """
+        Setup the OpenDA model factory, stoch observer and object function
+    
+        :return: a tuple containing an instance of the cost function, the initial parameters, an instance of the model and a stoch observer
+        """
+    
+    
+        p = self.model_instance.getParameters()        
+        return p
+    
+    def get_obs(self):
+        """
+        Find the observations and corresponding uncertainties at the relevant locations 
+        
+        :return: a tuple containing a list of the mean of all observations at each locations, followed by their respective standard deviations
+        """
+        
+        obs_mean = self.selection.getExpectations()
+        obs_mean = utils.input_to_py_list(obs_mean.getValues())
+        obs_std = self.selection.getStandardDeviations()
+        obs_std = utils.input_to_py_list(obs_std.getValues())
+        
+        return (obs_mean, obs_std)
+    
+    def object_function(self, p):
+        """
+        Compute the predictions of the object function for parameters p
+        :param p: parameters
+        :return: list of predictions
+        """
+    
+        # Create an OpenDA TreeVector with parameter value
+        p_new = utils.input_to_j_vector(p)
+        model_new = self.model_instance
+        model_new.setParameters(p_new)
+        model_new.compute(self.target_time)
+    
+        descr = self.selection.getObservationDescriptions()
+        prd = model_new.getObservationOperator().getObservedValues(descr)
+        prd = utils.input_to_py_list(prd.getValues())
+    
+        val = self.cost_function.evaluate(p_new, "-")
+        print("Cost="+str(val)+" p="+str(p))
+        return prd
--- py_openda/examples/swan_calibration_dud/testDud.py
+++ py_openda/examples/swan_calibration_dud/testDud.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Main function for using the dud algorithm to find the parameters corresponding
+to the optimal value of a function. If the function you want to use does not reqire
+py4j, you can comment out the line 'py_p0 = utils.input_to_py_list(py_p0)' if it causes trouble.
+
 Created on Wed Oct 10 14:40:17 2018
 
 @author: hegeman
@@ -8,22 +12,28 @@ Created on Wed Oct 10 14:40:17 2018
 
 #FIXME: netjes tussen Python en Java objecten wisselen.
 
-from py_openda.costFunctions import Kwadratic
+#from py_openda.costFunctions.PyKwadratic import PyKwadratic as Kwadratic
+from py_openda.costFunctions.Kwadratic import Kwadratic
 from py_openda.algorithms import Dud
 
-def main():
-    (cost_function, model_params, model_ini, observer) = Kwadratic.setup()
-    j_p0 = model_params.clone().getValues()
-    py_p0 = Kwadratic.j_array_to_py_list(j_p0)
-#    py_p0 = model_params
-    (obs_mean, obs_std) = Kwadratic.get_obs()
-    results = Dud.dud(Kwadratic.object_function,  py_p0, obs_mean, obs_std)
-#    results = dud_old(f, [2,-3], [1,2,2])
-#    results = dud_old(f2, [2,3], [1, 0.5, 0.5])
-#    results = dud_old(g, [2,-3], [0.909, -0.416, 0.841])
-    #print("Optimal value ="+str(results.x))
+import py_openda.utils.py4j_utils as utils
+
+def main(cost):
+    """
+    Runs the main dud algorithm for finding the optimal parameters for a certain costfunction.
+    :param cost: a model object which has the method get_parameters() which returns the parameters,
+    get_obs() for getting the observations with standard deviations, and object_function(p) which returns
+    the value for parameters p.
+    :return: tuple containing the minimal cost followed by the list of corresponding parameters
+    """
+    py_p0 = cost.get_parameters()
+    py_p0 = utils.input_to_py_list(py_p0)
+    (obs_mean, obs_std) = cost.get_obs()
+    results = Dud.dud(cost.object_function,  py_p0, obs_mean, obs_std)
+    return results
+if __name__ == "__main__":
+    cost = Kwadratic()
+    results = main(cost)
     print("Optimal value = "+str(results[0]))
     print("with the parameters "+str(results[1]))
     print("Done")
-if __name__ == "__main__":
-    main()
