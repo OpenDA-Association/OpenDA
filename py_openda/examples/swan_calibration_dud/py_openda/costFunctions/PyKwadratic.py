5dee9116c2284c5b466564cf0a7ac096a58d02b9
Rick Hegeman <rick_pc_hegeman@hotmail.com>
2018-11-29 12:03:22 +0100

    Included the utility functions in the py_openda library. Also tidied up the code a bit.

--- py_openda/examples/swan_calibration_dud/py_openda/costFunctions/PyKwadratic.py
+++ py_openda/examples/swan_calibration_dud/py_openda/costFunctions/PyKwadratic.py
@@ -5,56 +5,42 @@ Created on Fri Oct 12 15:06:13 2018
 
 @author: hegeman
 """
-import os
-os.chdir(os.getcwd()+'/py_openda/costFunctions')
-from simulation_kwadratic_cost_function import simulation_kwadratic_cost_function
-from model_instance import model_instance
-from stoch_observer import stoch_observer
-os.chdir(os.getcwd())
-cost_function = None
-model_params = None
-model_ini = None
-observer = None
+from py_openda.costFunctions.simulation_kwadratic_cost_function import simulation_kwadratic_cost_function
+from py_openda.costFunctions.model_instance import model_instance
+from py_openda.costFunctions.stoch_observer import stoch_observer
 
-def setup():
-    global cost_function
-    global model_params
-    global model_ini
-    global observer
-  
-    model_ini = model_instance()
-    observer = stoch_observer()
-    cost_function = simulation_kwadratic_cost_function(model_ini, observer)
-    p = model_ini.get_parameters()
-    model_params = p
+class PyKwadratic:
     
-    return(cost_function, model_params, model_ini, observer)
+    def __init__(self):
+        self.model_instance = model_instance()
+        self.observer = stoch_observer()
+        self.cost_function = simulation_kwadratic_cost_function(self.model_instance, self.observer)
+        
+    def get_parameters(self):
+        p = self.model_instance.get_parameters()
+        
+        return p
+        
+    def get_obs(self):
+        """
+        Find the observations and corresponding uncertainties at the relevant locations 
+        
+        :return: a tuple containing a list of the mean of all observations at each locations, followed by their respective standard deviations
+        """
+        selectionTimes = self.model_instance.get_time_horizon()
+        self.observer.create_selection(selectionTimes)
+        obs_mean = self.observer.get_expectations()
+        obs_std = self.observer.get_standard_deviations()
+        
+        return (obs_mean, obs_std)
     
-def get_obs():
-    """
-    Find the observations and corresponding uncertainties at the relevant locations 
-    
-    :return: a tuple containing a list of the mean of all observations at each locations, followed by their respective standard deviations
-    """
-    selectionTimes = model_ini.get_time_horizon()
-    observer.create_selection(selectionTimes)
-    obs_mean = observer.get_expectations()
-    obs_std = observer.get_standard_deviations()
-    
-    return (obs_mean, obs_std)
-
-def object_function(p):
-    """
-    Compute the predictions of the object function for parameters p
-    :param p: parameters
-    :return: list of predictions
-    """
-
-    # Create an OpenDA TreeVector with parameter value
-    model_new = model_ini
-    model_new.set_parameters(p)
-
-    prd = cost_function.evaluate(p)
-    print("Cost="+str(prd)+" p="+str(p))
-    return prd
+    def object_function(self, p):
+        """
+        Compute the predictions of the object function for parameters p
+        :param p: parameters
+        :return: list of predictions
+        """    
+        prd = self.cost_function.evaluate(p)
+        print("Cost="+str(prd)+" p="+str(p))
+        return prd
 
