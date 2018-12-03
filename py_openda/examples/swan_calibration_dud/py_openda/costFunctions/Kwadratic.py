541a16b178ae9627eb260b4a83602d1cadcdc4fe
Rick Hegeman <rick_pc_hegeman@hotmail.com>
2018-12-03 14:55:41 +0100

    Clean up of the main modules to make it a bit easier to look at

--- py_openda/examples/swan_calibration_dud/py_openda/costFunctions/Kwadratic.py
+++ py_openda/examples/swan_calibration_dud/py_openda/costFunctions/Kwadratic.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Module for letting a Python script use the Java object SimulationKwadraticCostFunction which is included in OpenDA.
-Every function below setup() uses global variables calculated by setup(), so when using this module
-it is recomended that setup() is called first.
+Class for letting a Python script use the Java object SimulationKwadraticCostFunction
+which is included in OpenDA.
+
 Created on Wed Oct 10 14:39:10 2018
 
 @author: hegeman
@@ -28,30 +28,33 @@ scriptdir = os.getcwd()
 
 
 class Kwadratic:
-    
+    """
+    Class for keeping track of the model and the stochastic observer.
+    """
     def __init__(self):
-        
+        """
+        """
         #Initialize the model factory
         #model_input_dir = "/Users/nils/Develop/py4j/swanModel/config"
         model_input_dir = os.path.join(scriptdir, 'swanModel', 'config')
         model_config_xml = "swanStochModelConfig.xml"
-    
-    
+
+
         model_factory = gateway.jvm.org.openda.model_swan.SwanCalibStochModelFactory()
         utils.initialize_openda_configurable(model_factory, model_input_dir, model_config_xml)
-    
+
         #Initialize stoch observer
         #observer_input_dir = "/Users/nils/Develop/py4j/stochObserver"
         observer_input_dir = model_input_dir = os.path.join(scriptdir, 'stochObserver')
         observer_config_xml = "swanStochObsConfig.xml"
-    
+
         observer = gateway.jvm.org.openda.observers.IoObjectStochObserver()
         utils.initialize_openda_configurable(observer, observer_input_dir, observer_config_xml)
-        
-        
+
+
         #Initialize cost function org.openda.algorithms
         self.cost_function = gateway.jvm.org.openda.algorithms.SimulationKwadraticCostFunction(model_factory, observer)
-    
+
         #Get initial parameter
         outputLevel = gateway.jvm.org.openda.interfaces.IStochModelFactory.OutputLevel.Debug
         self.model_instance = model_factory.getInstance(outputLevel)
@@ -61,46 +64,48 @@ class Kwadratic:
 
     def get_parameters(self):
         """
-        Setup the OpenDA model factory, stoch observer and object function
-    
-        :return: a tuple containing an instance of the cost function, the initial parameters, an instance of the model and a stoch observer
+        Extracts the parameters from the main model instance.
+
+        :return: the initial parameters.
         """
-    
-    
-        p = self.model_instance.getParameters()        
+
+
+        p = self.model_instance.getParameters()
         return p
-    
+
     def get_obs(self):
         """
-        Find the observations and corresponding uncertainties at the relevant locations 
-        
-        :return: a tuple containing a list of the mean of all observations at each locations, followed by their respective standard deviations
+        Find the observations and corresponding uncertainties at the relevant locations.
+
+        :return: a tuple containing a list of the mean of all observations at each locations,
+        followed by their respective standard deviations.
         """
-        
+
         obs_mean = self.selection.getExpectations()
         obs_mean = utils.input_to_py_list(obs_mean.getValues())
         obs_std = self.selection.getStandardDeviations()
         obs_std = utils.input_to_py_list(obs_std.getValues())
-        
+
         return (obs_mean, obs_std)
-    
+
     def object_function(self, p):
         """
-        Compute the predictions of the object function for parameters p
-        :param p: parameters
-        :return: list of predictions
+        Compute the predictions of the object function for parameters p.
+        :param p: parameters.
+        :return: list of predictions.
         """
-    
+
         # Create an OpenDA TreeVector with parameter value
         p_new = utils.input_to_j_vector(p)
         model_new = self.model_instance
         model_new.setParameters(p_new)
         model_new.compute(self.target_time)
-    
+
         descr = self.selection.getObservationDescriptions()
         prd = model_new.getObservationOperator().getObservedValues(descr)
         prd = utils.input_to_py_list(prd.getValues())
-    
+
         val = self.cost_function.evaluate(p_new, "-")
         print("Cost="+str(val)+" p="+str(p))
         return prd
+    
