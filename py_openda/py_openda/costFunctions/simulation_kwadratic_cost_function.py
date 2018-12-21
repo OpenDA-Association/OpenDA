ad9bad9f2021ed04f5df6215439a956df32a5ea8
Rick Hegeman <rick_pc_hegeman@hotmail.com>
2018-12-21 16:39:47 +0100

    Moved Python library and added TODO's

---
+++ py_openda/py_openda/costFunctions/simulation_kwadratic_cost_function.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 12 13:44:45 2018
+
+@author: hegeman
+"""
+
+class simulation_kwadratic_cost_function:
+    def __init__(self, model_instance, stoch_observer):
+        self.model_instance = model_instance
+        self.stoch_observer = stoch_observer
+        
+    def evaluate(self, parameters):
+        self.model_instance.set_parameters(parameters)
+        sel = self.stoch_observer.get_observation_descriptions()
+        test = self.model_instance.get_observed_values(sel)
+        return test
