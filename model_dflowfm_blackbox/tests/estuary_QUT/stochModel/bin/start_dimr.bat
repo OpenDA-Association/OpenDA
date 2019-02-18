6982f3f09259c261bbbdccde969fcf63e3d5f773
vrieling <alja.vrieling@vortech.nl>
2019-02-18 17:01:43 +0100

    Update input files for DFlowFM

---
+++ model_dflowfm_blackbox/tests/estuary_QUT/stochModel/bin/start_dimr.bat
@@ -0,0 +1,5 @@
+rem cd dflowfm
+rem "C:\Program Files (x86)\Deltares\Delft3D FM Suite 2017 HMWQ (1.2.2.36603)\plugins\DeltaShell.Dimr\run_dimr.bat" dimrConfigEstuary.xml
+rem "C:\Program Files (x86)\Deltares\Delft3D FM Suite 2018.01 HMWQ (1.4.3.39355)\plugins\DeltaShell.Dimr\kernels\x64\dimr\scripts\run_dimr.bat dimrConfigEstuary.xml" 
+"c:\Program Files (x86)\Deltares\Delft3D FM Suite 2019.01 HMWQ (1.5.1.41875)\plugins\DeltaShell.Dimr\kernels\x64\dimr\scripts\run_dimr.bat" dimrConfigEstuary.xml
+rem cd ..
