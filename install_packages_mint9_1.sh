d4aa840b9bbdc530ea4e9b91bcd4562bf050e23a
Nils van Velzen <nilsvanvelzen@gmail.com>
2019-05-21 19:09:25 +0200

    Added install scripts for linux packages (mint 9.1 and travis)

---
+++ install_packages_mint9_1.sh
@@ -0,0 +1,14 @@
+# All packages needed in order to compile OpenDA
+# version in Linux mint 9.1
+#
+sudo apt install vim
+sudo apt install default-jdk
+sudo apt install ant-optional
+sudo apt install libsqlite3-dev
+sudo apt install gfortran
+sudo apt install g++
+sudo apt install mpi-default-dev
+sudo apt install libnetcdf-dev
+sudo apt install liblapack-dev
+sudo apt install libxml2
+sudo apt install junit-dev
--- travis_install.sh
+++ travis_install.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
-sudo apt-get install gfortran
-sudo apt-get install mpi-default-dev
-sudo apt-get install libnetcdf-dev
-sudo apt-get install liblapack-dev
+echo INSTALL EXTRA SOFTWARE
+bash install_packages_travis.sh
 cd ..
 mv OpenDA public
 cd public
 
 echo START BUILDING NATIVE CODE
-echo "current location $PWD"
 ls 
 bash  install_native.sh
 
