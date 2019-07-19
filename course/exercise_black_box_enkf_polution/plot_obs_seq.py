a8b70e8ee1561e3b2fc9532fd3ad5fad70b6bdf3
robot144 <martin@verlaan.info>
2019-07-19 16:25:28 +0200

    plot_obs_seq.py script in course/exercise_black_box_enkf_polution was assuming 3 observation locations, but the openda config has only one. Script now works with 1,2 or 2 locations, but may still fail in more complicated scenarios.

--- course/exercise_black_box_enkf_polution/plot_obs_seq.py
+++ course/exercise_black_box_enkf_polution/plot_obs_seq.py
@@ -1,17 +1,20 @@
 #! /usr/bin/env python3
-import sequentialSimulation_results as seq_sim
+#import sequentialSimulation_results as sim
+import enkf_results as sim
 import matplotlib.pyplot as plt
 
-obs = seq_sim.obs
-pred = seq_sim.pred_f_central
-time = seq_sim.analysis_time
+
+obs = sim.obs
+nloc=obs.shape[1]//2 #number of observation locations included in OpenDA run
+pred = sim.pred_f_central
+time = sim.analysis_time
 
 f,ax = plt.subplots(3,2)
-for i in range(3):
+for i in range(nloc):    
    ax[i,0].plot(time,obs[:,i],'r')
    ax[i,0].plot(time,pred[:,i],'b')
-   ax[i,1].plot(time,obs[:,i+3],'r')
-   ax[i,1].plot(time,pred[:,i+3],'b')
+   ax[i,1].plot(time,obs[:,i+nloc],'r')
+   ax[i,1].plot(time,pred[:,i+nloc],'b')
 ax[0,0].set_title("c1")
 ax[0,1].set_title("c2")
 plt.show()
