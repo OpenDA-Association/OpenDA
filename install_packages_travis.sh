6c1ae16ff9c7ad774496f3dc4a03f5911765c124
Erik Pelgrim <39620557+erikpelgrim@users.noreply.github.com>
2019-09-16 15:09:37 +0200

    Fix travis

---
+++ install_packages_travis.sh
@@ -0,0 +1,8 @@
+# All packages needed in order to compile OpenDA
+# Travis virtual machine
+#
+sudo apt-get install gfortran
+sudo apt-get install mpi-default-dev
+sudo apt-get install libnetcdf-dev
+sudo apt-get install liblapack-dev
+sudo apt-get install ant
