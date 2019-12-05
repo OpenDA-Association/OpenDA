b7ccd44b79a0f4653bc1fa4e692bea03c201224f
Erik Pelgrim <39620557+erikpelgrim@users.noreply.github.com>
2019-12-05 16:25:23 +0100

    Add logging for debugging non responsive Socket

--- model_external_socket/java/src/org/openda/externalsocket/SocketClient.java
+++ model_external_socket/java/src/org/openda/externalsocket/SocketClient.java
@@ -22,7 +22,11 @@ class SocketClient {
 			Socket echoSocket = new Socket(hostAddress, port);
 			PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true)) {
 			out.println(messageIn);
-			BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+			System.out.println("Message sent: " + messageIn);
+			InputStream inputStream = echoSocket.getInputStream();
+			System.out.println("Input stream available: " + inputStream.available());
+			BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+			System.out.println("Start reading line from input stream...");
 			return in.readLine();
 		} catch (UnknownHostException e) {
 			System.err.println("Don't know about host " + hostAddress);
