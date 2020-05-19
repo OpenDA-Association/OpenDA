124ab944df0267acc53604470088230c958214a5
theavuik <thea.vuik@vortech.nl>
2020-05-19 10:29:05 +0200

    AbstractDataObject for ResultFileWrapper

--- model_RainfallRunoffZhang/java/src/org/openda/model_RainfallRunoffZhang/ResultFileWrapper.java
+++ model_RainfallRunoffZhang/java/src/org/openda/model_RainfallRunoffZhang/ResultFileWrapper.java
@@ -18,21 +18,13 @@
 * along with OpenDA.  If not, see <http://www.gnu.org/licenses/>.
 */
 package org.openda.model_RainfallRunoffZhang;
-import java.io.BufferedReader;
-//import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-//import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Set;
-import java.util.Vector;
 
-import org.openda.blackbox.interfaces.IoObjectInterface;
+import org.openda.exchange.AbstractDataObject;
 import org.openda.exchange.timeseries.TimeSeries;
 import org.openda.interfaces.IExchangeItem;
+
+import java.io.*;
+import java.util.Vector;
 
 /**
  * Allows reading of time variables from matlab readable ASCII file and
@@ -54,107 +46,42 @@ import org.openda.interfaces.IExchangeItem;
  * @author Beatrice Marti, hydrosolutions ltd.
  *
  */
-public class ResultFileWrapper implements IoObjectInterface {
+public class ResultFileWrapper extends AbstractDataObject {
 
 	// Class specific values
 	File workingDir;
-	String configString;
 	String fileName = null;
-	HashMap<String, TimeSeries> items = new LinkedHashMap<String, TimeSeries>();
-	
+
 	/**
-	 * Initialize the IoObject. Reads the content of a file (fileName) in
+	 * Initialize the IoObject. Reads the content of a file in
 	 * directory (workingDir) with given arguments.
 	 * 
 	 * @param workingDir
 	 *            Working directory
-	 * @param fileName
-	 *            The name of the file containing the data (relative to the
-	 *            working directory.)
 	 * @param arguments
-	 *            Additional arguments (may be null zero-length)
+	 *            The name of the file containing the data (relative to the
+	 *            working directory), and additional arguments (may be null zero-length)
 	 */
-	public void initialize(File workingDir, String fileName, String[] arguments) {
+	@Override
+	public void initialize(File workingDir, String[] arguments) {
 		
 		this.workingDir = workingDir;
-		this.fileName = fileName;
+		this.fileName = arguments[0];
 		
 		try {
-			ReadNameListFile(workingDir, fileName, arguments);
+			ReadNameListFile();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	/**
-	 * Ask which elements can be accessed
-	 * 
-	 * 
-	 * @return The list of element identifiers that can be accessed
-	 */
-	public TimeSeries[] getExchangeItems() {
-		// Get the number of items.
-		int n = this.items.size();
-		Set<String> keys = this.items.keySet();
-		TimeSeries[] result = new TimeSeries[n];
-		int i = 0;
-		for (String key : keys) {
-			result[i] = this.items.get(key);
-			i++;
-		}
-		return result;
-	}
-	
-	/** 
-	 * A slightly more elegant variant of getExchangeItems().
-	 * 
-	 * @return A vector of type TimeSeries containing the values of the 
-	 *         time series stored in the hash map items.
-	 */
-	public Vector<TimeSeries> getData() {
-		return new Vector<TimeSeries>(this.items.values());
-	}
-	
-	
+	@Override
 	public void finish() {
 		/*
 		 * In this example, writing the result file is not needed because the totalRunoff is not 
 		 * read by the rainfall runoff model to initialize a run. 
 		 * 
-		 * 
-		// Write updated predictor
-		double[] currentTimeCache = this.items.get("totalRunoff").getTimes();
-		double[] totalRunoffCache = this.items.get("totalRunoff").getValuesAsDoubles();
-		
-		//write to file
-		System.out.println("Result wrapper finish(): Writing to file: "+this.workingDir+"/"+this.fileName);
-		File outputFile = new File(this.workingDir,this.fileName);
-		try{
-			if(outputFile.isFile()){
-				outputFile.delete();
-			}
-		}catch (Exception e) {
-			System.out.println("ResultWrapper: trouble removing file "+ fileName);
-		}
-		try {
-			FileWriter writer = new FileWriter(outputFile);
-			BufferedWriter out = new BufferedWriter(writer);
-
-			int seriesLength = currentTimeCache.length;
-			if (seriesLength != totalRunoffCache.length) {
-				System.out.println("Error writing result file. Length of times and values are not the same.");
-			}
-			for (int i = 0 ; i < seriesLength; i++) {
-				out.write(currentTimeCache[i] + " " + totalRunoffCache[i] + System.getProperty("line.separator"));
-			}
-			out.flush(); // Force java to write buffered stream to disk.
-			out.close();
-			writer.close();
-
-		} catch (Exception e) {
-			throw new RuntimeException("Result file wrapper: Problem writing to file "+fileName+" : " + System.getProperty("line.separator") + e.getMessage());
-		}
-		*/
+		 */
 	}
 
 	/**
@@ -162,26 +89,19 @@ public class ResultFileWrapper implements IoObjectInterface {
 	 * them to an ExchangeItem.
 	 * <p>
 	 * Actually reads one single value from file.
-	 * 
-	 * @param workingDir
-	 * @param fileName
-	 * @param arguments
+	 *
 	 * @throws IOException
 	 */
-	private void ReadNameListFile(File workingDir, String fileName,
-			String[] arguments) throws IOException {
-		
+	private void ReadNameListFile() throws IOException {
+
 		File namelist = new File(workingDir, fileName);
 		if (!namelist.exists()) {
 			System.out.println("ResultFileWrapper.ReadNameListFile(): Failed to open File " + fileName + " in " + workingDir);
 		}
-		//TimeSeries series = new TimeSeries();
-		Vector<Double> timesVector = new Vector<Double>();
-		Vector<Double> valuesVector = new Vector<Double>();
-		//ArrayList<Double> times = new ArrayList<Double>();
-		//ArrayList<Double> values = new ArrayList<Double>();
-		double[] times = null;
-		double[] values = null;
+		Vector<Double> timesVector = new Vector<>();
+		Vector<Double> valuesVector = new Vector<>();
+		double[] times;
+		double[] values;
 		
 		if (!namelist.exists()) {
 			throw new RuntimeException("ResultFileWrapper.ReadNameListFile(): settings file "
@@ -190,8 +110,8 @@ public class ResultFileWrapper implements IoObjectInterface {
 		// Create nested reader.
 		FileInputStream in = new FileInputStream(namelist);
 		BufferedReader buff = new BufferedReader(new InputStreamReader(in));
-		String line = ""; // Initialize line.
-		Boolean eof = false; // End of file cache.
+		String line; // Initialize line.
+		boolean eof = false; // End of file cache.
 
 		// While End of file is not reached yet do the following:
 		while (!eof) {
@@ -243,10 +163,7 @@ public class ResultFileWrapper implements IoObjectInterface {
 	    TimeSeries series = new TimeSeries(times, values);
 	    String id = "totalRunoff";
 	    series.setId(id);
-	    this.items.put(id,series);
-	    @SuppressWarnings("unused")
-		IExchangeItem totalRunoffExchangeItem = new TimeSeries(series);
-	    
+	    this.exchangeItems.put(id,series);
 	    
 	}
 
