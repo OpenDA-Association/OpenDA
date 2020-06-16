4481d6e27a866f3a216f07981e2f8b2b9b216643
theavuik <thea.vuik@vortech.nl>
2020-06-16 11:24:32 +0200

    AbstractDataObject for myWrapper

--- model_reactive_advection/java/test/org/openda/mywrapper/myWrapperTest.java
+++ model_reactive_advection/java/test/org/openda/mywrapper/myWrapperTest.java
@@ -18,9 +18,9 @@
 * along with OpenDA.  If not, see <http://www.gnu.org/licenses/>.
 */
 package org.openda.mywrapper;
+
 import junit.framework.TestCase;
 import org.openda.blackbox.config.BBUtils;
-import org.openda.blackbox.interfaces.IoObjectInterface;
 import org.openda.exchange.DoublesExchangeItem;
 import org.openda.exchange.timeseries.NoosTimeSeriesFormatter;
 import org.openda.exchange.timeseries.TimeSeries;
@@ -29,6 +29,7 @@ import org.openda.interfaces.IExchangeItem;
 import org.openda.model_reactive_advection.myCopier;
 import org.openda.model_reactive_advection.myWrapper;
 import org.openda.utils.OpenDaTestSupport;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -38,40 +39,34 @@ public class myWrapperTest extends TestCase {
 	private File testRunDataDir;
 	private OpenDaTestSupport testData;
 
-	protected void setUp() throws IOException {
+	protected void setUp() {
 		testData = new OpenDaTestSupport(myWrapperTest.class,"model_reactive_advection");
 		testRunDataDir = testData.getTestRunDataDir();
 	}
 
 	public void testReadInput() {
 
-		IoObjectInterface ioObject = new myWrapper();
-		String args[] = {};
-		ioObject.initialize(testRunDataDir, "reactive_pollution_model.input", args);
+		myWrapper wrapper = new myWrapper();
+		String[] args = {"reactive_pollution_model.input"};
+		wrapper.initialize(testRunDataDir, args);
 
-		IExchangeItem[] exchangeItems = ioObject.getExchangeItems();
+		String[] exchangeItemIds = wrapper.getExchangeItemIDs();
 
-        for(int item=0;item<exchangeItems.length;item++){
-
-			if(exchangeItems[item].getId().equalsIgnoreCase("source.factory1,discharge")){
-				IExchangeItem ex = (IExchangeItem)exchangeItems[0];
-				//String getId();
-				String id = ex.getId();
-				assertEquals("ex.getId()", "source.factory1.discharge", id);
-
-				//String getQuantityId();
+        for(String id:exchangeItemIds){
+        	IExchangeItem exchangeItem = wrapper.getDataObjectExchangeItem(id);
+        	String exId = exchangeItem.getId();
+        	assertEquals(id, exId);
+			if(exId.equalsIgnoreCase("source.factory1,discharge")){
+				IExchangeItem ex = wrapper.getDataObjectExchangeItem(exchangeItemIds[0]);
 				String quantityId = ((TimeSeries) ex).getQuantityId();
 				assertEquals("ex.getQuantityId()", "discharge", quantityId);
 
-				//String getUnitId();
 				String unitId = ((TimeSeries) ex).getUnitId();
 				assertEquals("ex.getUnitId()", "kg/s", unitId);
 
-				//public Type getObjectType(); //
 				IExchangeItem.ValueType valueType = ex.getValuesType();
-				assertTrue(valueType == IExchangeItem.ValueType.IArrayType);
+				assertSame(valueType, IExchangeItem.ValueType.IArrayType);
 
-				//public Object getValues();
 				TimeSeries seriesRef = (TimeSeries) ex.getValues();
 				double[] timesRef = seriesRef.getTimesRef();
 				assertEquals("times[0]", 51513.0, timesRef[0], 0.0001);
@@ -80,29 +75,29 @@ public class myWrapperTest extends TestCase {
 				assertEquals("values[1]", 0.0, valuesCopy[0], 0.0001);
 			}
 
-			if(exchangeItems[item] instanceof TimeSeries){
+			if(exchangeItem instanceof TimeSeries){
 				TimeSeriesFormatter noosFormatter = new NoosTimeSeriesFormatter();
-				TimeSeries series= (TimeSeries)exchangeItems[item];
+				TimeSeries series= (TimeSeries)exchangeItem;
 				noosFormatter.writeToStandardOut(series);
-			}else if(exchangeItems[item] instanceof DoublesExchangeItem){
-				System.out.println("Item["+exchangeItems[item].getId()+"] ="+ exchangeItems[item].toString());
+			}else if(exchangeItem instanceof DoublesExchangeItem){
+				System.out.println("Item["+exchangeItem.getId()+"] ="+ exchangeItem.toString());
 			}
 		}
 	}
 
 	public void testReadOutput() {
-		IoObjectInterface ioObject = new myWrapper();
-		String args[] = {};
-		ioObject.initialize(testRunDataDir, "reactive_pollution_model.output", args);
+		myWrapper wrapper = new myWrapper();
+		String[] args = {"reactive_pollution_model.output"};
+		wrapper.initialize(testRunDataDir, args);
 		//TODO fix data string
-		IExchangeItem[] exchangeItems = ioObject.getExchangeItems();
+		String[] exchangeItemIDs = wrapper.getExchangeItemIDs();
 		// TODO deze test heeft nooit iets gedaan omdat exchangeItems ids = output.c[1,2]_loc[A,B,C].concentration
-		for(int item=0;item<exchangeItems.length;item++){
-			if(exchangeItems[item].getId().equalsIgnoreCase("source.factory1,discharge")){
-				IExchangeItem ex = exchangeItems[0];
-				//String getId();
-				String id = ex.getId();
-				assertEquals("ex.getId()", "source.factory1.discharge", id);
+		for(String itemId:exchangeItemIDs){
+			IExchangeItem exchangeItem = wrapper.getDataObjectExchangeItem(itemId);
+			String exId = exchangeItem.getId();
+			assertEquals(itemId, exId);
+			if(exId.equalsIgnoreCase("source.factory1,discharge")){
+				IExchangeItem ex = wrapper.getDataObjectExchangeItem(exchangeItemIDs[0]);
 
 				//String getQuantityId();
 				String quantityId = ((TimeSeries) ex).getQuantityId();
@@ -112,7 +107,7 @@ public class myWrapperTest extends TestCase {
 				String unitId = ((TimeSeries) ex).getUnitId();
 				assertEquals("ex.getUnitId()", "kg/s", unitId);
 
-				//TODO als IExchangeItem ex dan ex.getVakuesType() test toevoegen
+				//TODO als IExchangeItem ex dan ex.getValuesType() test toevoegen
 				// public Type getObjectType();
 				//Type valueType = ex.getValueType();
 				//assertTrue(valueType == org.openda.exchange.timeseries.TimeSeries.class);
@@ -126,17 +121,17 @@ public class myWrapperTest extends TestCase {
 				assertEquals("values[1]", 0.0, valuesCopy[0], 0.0001);
 			}
 
-			if(exchangeItems[item] instanceof TimeSeries){
+			if(exchangeItem instanceof TimeSeries){
 				TimeSeriesFormatter noosFormatter = new NoosTimeSeriesFormatter();
-				noosFormatter.writeToStandardOut((TimeSeries)exchangeItems[item]);
+				noosFormatter.writeToStandardOut((TimeSeries)exchangeItem);
 			}
 		}
 	}
 
 	public void testWriteInput() {
 		//First read input
-		IoObjectInterface ioObject = new myWrapper();
-		String args[] = {};
+		myWrapper wrapper = new myWrapper();
+		String[] args = {"reactive_pollution_model_copy.input"};
 		File original = new File(testRunDataDir,"reactive_pollution_model.input");
 		File copy = new File(testRunDataDir,"reactive_pollution_model_copy.input");
 		try {
@@ -144,42 +139,45 @@ public class myWrapperTest extends TestCase {
 		} catch (IOException e) {
 			throw new RuntimeException("Could not copy file "+original.getAbsolutePath()+" to "+copy.getAbsolutePath());
 		}
-		ioObject.initialize(testRunDataDir, "reactive_pollution_model_copy.input", args);
+		wrapper.initialize(testRunDataDir, args);
 
-		IExchangeItem[] exchangeItems = ioObject.getExchangeItems();
+		String[] exchangeItemIDs = wrapper.getExchangeItemIDs();
 
 		//change some things
-		for(int item=0;item<exchangeItems.length;item++){
-			System.out.println("item = "+item+" id="+exchangeItems[item].getId());
-			if(exchangeItems[item].getId().equalsIgnoreCase("concentration1.grid")){
-				double values[] = exchangeItems[item].getValuesAsDoubles();
+		for(String id:exchangeItemIDs){
+			IExchangeItem exchangeItem = wrapper.getDataObjectExchangeItem(id);
+			String exId = exchangeItem.getId();
+			assertEquals(id, exId);
+			System.out.println("exchange item id = " + exId);
+			if(exId.equalsIgnoreCase("concentration1.grid")){
+				double[] values = exchangeItem.getValuesAsDoubles();
 				values[0]= 100.0;
-				exchangeItems[item].setValuesAsDoubles(values);
+				exchangeItem.setValuesAsDoubles(values);
 			}
-			if(exchangeItems[item].getId().equalsIgnoreCase("source.c1_factory1.discharge")){
-				double values[] = exchangeItems[item].getValuesAsDoubles();
+			if(exId.equalsIgnoreCase("source.c1_factory1.discharge")){
+				double[] values = exchangeItem.getValuesAsDoubles();
 				values[0]= 123.0;
-				exchangeItems[item].setValuesAsDoubles(values);
+				exchangeItem.setValuesAsDoubles(values);
 			}
-			if(exchangeItems[item].getId().equalsIgnoreCase("bound.c1_left.concentration")){
-				double values[] = exchangeItems[item].getValuesAsDoubles();
+			if(exId.equalsIgnoreCase("bound.c1_left.concentration")){
+				double[] values = exchangeItem.getValuesAsDoubles();
 				values[0]= 10.0;
-				exchangeItems[item].setValuesAsDoubles(values);
+				exchangeItem.setValuesAsDoubles(values);
 			}
-			if(exchangeItems[item].getId().equalsIgnoreCase("output.c1_locA.concentration")){
-				double values[] = exchangeItems[item].getValuesAsDoubles();
+			if(exId.equalsIgnoreCase("output.c1_locA.concentration")){
+				double[] values = exchangeItem.getValuesAsDoubles();
 				values[0]= 102030.0;
-				exchangeItems[item].setValuesAsDoubles(values);
+				exchangeItem.setValuesAsDoubles(values);
 			}
-			if(exchangeItems[item].getId().equalsIgnoreCase("reactionTime")){
-				double values[] = exchangeItems[item].getValuesAsDoubles();
+			if(exId.equalsIgnoreCase("reactionTime")){
+				double[] values = exchangeItem.getValuesAsDoubles();
 				values[0]= 700.0;
-				exchangeItems[item].setValuesAsDoubles(values);
+				exchangeItem.setValuesAsDoubles(values);
 			}
 		}
 
 		//write to file
-		ioObject.finish();
+		wrapper.finish();
 		boolean containsLocA =testData.FileContains(copy, "102030");
 		assertTrue(containsLocA);
 
@@ -189,7 +187,7 @@ public class myWrapperTest extends TestCase {
 	}
 
 	public void testCopyState() {
-		String args[] = new String[2];
+		String[] args = new String[2];
 		File original = new File(testRunDataDir,"reactive_pollution_model.input");
 		File destination = new File(testRunDataDir,"reactive_pollution_model_copy3.input");
 		try {
