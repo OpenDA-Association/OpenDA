e17fe48f844cb041fbf271e9df91d8fc62d3184b
Stef <stef.hummel@deltares.nl>
2020-11-24 17:43:39 +0100

    Fixed xml-parsing for dataObject and IoObject based stochObserver

--- observers/java/src/org/openda/observers/IoObjectStochObserverConfigReader.java
+++ observers/java/src/org/openda/observers/IoObjectStochObserverConfigReader.java
@@ -19,8 +19,8 @@
 */
 package org.openda.observers;
 
-import org.openda.observers.io.castorgenerated.IoObjectStochObserverXML;
-import org.openda.observers.io.castorgenerated.StochObsIoObjectXML;
+import org.openda.observers.io.castorgenerated.DataObjectStochObserverXML;
+import org.openda.observers.io.castorgenerated.StochObsDataObjectXML;
 import org.openda.observers.io.castorgenerated.StochObsUncertaintyModuleXML;
 import org.openda.utils.io.CastorUtils;
 
@@ -34,54 +34,54 @@ import java.util.List;
  */
 public class IoObjectStochObserverConfigReader {
 
-    private IoObjectStochObserverConfig ioObjectStochObserverConfig;
+    private DataObjectStochObserverConfig dataObjectStochObserverConfig;
 
     public IoObjectStochObserverConfigReader(File configFile) {
 
         // xml-parse
-        org.openda.observers.io.castorgenerated.IoObjectStochObserverXML ioObjectStochObserverXML =
-                (IoObjectStochObserverXML) CastorUtils.parse(configFile, IoObjectStochObserverXML.class);
+        org.openda.observers.io.castorgenerated.DataObjectStochObserverXML dataObjectStochObserverXML =
+                (DataObjectStochObserverXML) CastorUtils.parse(configFile, DataObjectStochObserverXML.class);
 
         // uncertainty module
-        StochObsUncertaintyModuleXML uncertaintyModuleXML = ioObjectStochObserverXML.getUncertaintyModule();
-        IoObjectStochObserverConfig.IoStochObsUncertaintyConfig uncertaintyModuleConfig = new IoObjectStochObserverConfig.IoStochObsUncertaintyConfig(
+        StochObsUncertaintyModuleXML uncertaintyModuleXML = dataObjectStochObserverXML.getUncertaintyModule();
+        DataObjectStochObserverConfig.DataStochObsUncertaintyConfig uncertaintyModuleConfig = new DataObjectStochObserverConfig.DataStochObsUncertaintyConfig(
                 new File(configFile.getParentFile(),uncertaintyModuleXML.getWorkingDirectory()),
                 uncertaintyModuleXML.getClassName(), uncertaintyModuleXML.getArg());
 
         // io objects
-        ArrayList<IoObjectStochObserverConfig.IoStochObsIoObjectConfig> ioObjectConfigs = new ArrayList<IoObjectStochObserverConfig.IoStochObsIoObjectConfig>();
-        for (StochObsIoObjectXML stochObsIoObjectXML : ioObjectStochObserverXML.getIoObject()) {
+        ArrayList<DataObjectStochObserverConfig.IoStochObsIoObjectConfig> ioObjectConfigs = new ArrayList<DataObjectStochObserverConfig.IoStochObsIoObjectConfig>();
+        for (StochObsDataObjectXML stochObsDataObjectXML : dataObjectStochObserverXML.getDataObject()) {
             double missingValue;
-            if (stochObsIoObjectXML.hasMissingValue()){
-                missingValue = stochObsIoObjectXML.getMissingValue();
+            if (stochObsDataObjectXML.hasMissingValue()){
+                missingValue = stochObsDataObjectXML.getMissingValue();
             } else {
                 missingValue = Double.NaN;
             }
-            ioObjectConfigs.add(new IoObjectStochObserverConfig.IoStochObsIoObjectConfig(
-                new File(configFile.getParentFile(),stochObsIoObjectXML.getWorkingDirectory()),
-                stochObsIoObjectXML.getClassName(), stochObsIoObjectXML.getFileName(),
-                stochObsIoObjectXML.getArg(),missingValue));
+            ioObjectConfigs.add(new DataObjectStochObserverConfig.IoStochObsIoObjectConfig(
+                new File(configFile.getParentFile(),stochObsDataObjectXML.getWorkingDirectory()),
+                stochObsDataObjectXML.getClassName(), stochObsDataObjectXML.getFileName(),
+                stochObsDataObjectXML.getArg(),missingValue));
         }
 
         // assimilation and validation station identifiers
 		List<String> assimilationObsIds = new ArrayList<String>();
-		if (ioObjectStochObserverXML.getAssimilationStations() != null) {
-			Collections.addAll(assimilationObsIds, ioObjectStochObserverXML.getAssimilationStations().getObservationId());
+		if (dataObjectStochObserverXML.getAssimilationStations() != null) {
+			Collections.addAll(assimilationObsIds, dataObjectStochObserverXML.getAssimilationStations().getObservationId());
 		}
 		List<String> validationObsIds = new ArrayList<String>();
-		if (ioObjectStochObserverXML.getValidationStations() != null) {
-			Collections.addAll(validationObsIds, ioObjectStochObserverXML.getValidationStations().getObservationId());
+		if (dataObjectStochObserverXML.getValidationStations() != null) {
+			Collections.addAll(validationObsIds, dataObjectStochObserverXML.getValidationStations().getObservationId());
 		}
 
         // should missing values be removed?
-        boolean removeMissingValues = ioObjectStochObserverXML.getRemoveMissingValues();
+        boolean removeMissingValues = dataObjectStochObserverXML.getRemoveMissingValues();
 
         // store config
-		ioObjectStochObserverConfig = new IoObjectStochObserverConfig(uncertaintyModuleConfig, ioObjectConfigs,
+		dataObjectStochObserverConfig = new DataObjectStochObserverConfig(uncertaintyModuleConfig, ioObjectConfigs,
 				assimilationObsIds, validationObsIds, removeMissingValues);
     }
 
-    public IoObjectStochObserverConfig getIoObjectStochObserverConfig() {
-        return ioObjectStochObserverConfig;
+    public DataObjectStochObserverConfig getDataObjectStochObserverConfig() {
+        return dataObjectStochObserverConfig;
     }
 }
