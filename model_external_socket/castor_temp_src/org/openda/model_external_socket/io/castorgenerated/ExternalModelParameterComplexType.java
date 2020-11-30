82e609ffca064acd7abdd2f480b1f754fe48bcc4
Erik Pelgrim <39620557+erikpelgrim@users.noreply.github.com>
2020-11-30 17:13:39 +0100

    Second attempt to fix travis

---
+++ model_external_socket/castor_temp_src/org/openda/model_external_socket/io/castorgenerated/ExternalModelParameterComplexType.java
@@ -0,0 +1,281 @@
+/*
+ * This class was automatically generated with 
+ * <a href="http://www.castor.org">Castor 0.9.4.3</a>, using an XML
+ * Schema.
+ * $Id$
+ */
+
+package org.openda.model_external_socket.io.castorgenerated;
+
+  //---------------------------------/
+ //- Imported classes and packages -/
+//---------------------------------/
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Serializable;
+import java.io.Writer;
+import org.exolab.castor.xml.MarshalException;
+import org.exolab.castor.xml.Marshaller;
+import org.exolab.castor.xml.Unmarshaller;
+import org.exolab.castor.xml.ValidationException;
+import org.xml.sax.ContentHandler;
+
+/**
+ * Class ExternalModelParameterComplexType.
+ * 
+ * @version $Revision$ $Date$
+ */
+public class ExternalModelParameterComplexType implements java.io.Serializable {
+
+
+      //--------------------------/
+     //- Class/Member Variables -/
+    //--------------------------/
+
+    /**
+     * Field _startValue
+     */
+    private double _startValue;
+
+    /**
+     * keeps track of state for field: _startValue
+     */
+    private boolean _has_startValue;
+
+    /**
+     * Field _stdDev
+     */
+    private double _stdDev;
+
+    /**
+     * keeps track of state for field: _stdDev
+     */
+    private boolean _has_stdDev;
+
+    /**
+     * Field _lowerBound
+     */
+    private double _lowerBound;
+
+    /**
+     * keeps track of state for field: _lowerBound
+     */
+    private boolean _has_lowerBound;
+
+    /**
+     * Field _upperBound
+     */
+    private double _upperBound;
+
+    /**
+     * keeps track of state for field: _upperBound
+     */
+    private boolean _has_upperBound;
+
+
+      //----------------/
+     //- Constructors -/
+    //----------------/
+
+    public ExternalModelParameterComplexType() {
+        super();
+    } //-- org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType()
+
+
+      //-----------/
+     //- Methods -/
+    //-----------/
+
+    /**
+     * Method deleteLowerBound
+     */
+    public void deleteLowerBound()
+    {
+        this._has_lowerBound= false;
+    } //-- void deleteLowerBound() 
+
+    /**
+     * Method deleteUpperBound
+     */
+    public void deleteUpperBound()
+    {
+        this._has_upperBound= false;
+    } //-- void deleteUpperBound() 
+
+    /**
+     * Returns the value of field 'lowerBound'.
+     * 
+     * @return the value of field 'lowerBound'.
+     */
+    public double getLowerBound()
+    {
+        return this._lowerBound;
+    } //-- double getLowerBound() 
+
+    /**
+     * Returns the value of field 'startValue'.
+     * 
+     * @return the value of field 'startValue'.
+     */
+    public double getStartValue()
+    {
+        return this._startValue;
+    } //-- double getStartValue() 
+
+    /**
+     * Returns the value of field 'stdDev'.
+     * 
+     * @return the value of field 'stdDev'.
+     */
+    public double getStdDev()
+    {
+        return this._stdDev;
+    } //-- double getStdDev() 
+
+    /**
+     * Returns the value of field 'upperBound'.
+     * 
+     * @return the value of field 'upperBound'.
+     */
+    public double getUpperBound()
+    {
+        return this._upperBound;
+    } //-- double getUpperBound() 
+
+    /**
+     * Method hasLowerBound
+     */
+    public boolean hasLowerBound()
+    {
+        return this._has_lowerBound;
+    } //-- boolean hasLowerBound() 
+
+    /**
+     * Method hasStartValue
+     */
+    public boolean hasStartValue()
+    {
+        return this._has_startValue;
+    } //-- boolean hasStartValue() 
+
+    /**
+     * Method hasStdDev
+     */
+    public boolean hasStdDev()
+    {
+        return this._has_stdDev;
+    } //-- boolean hasStdDev() 
+
+    /**
+     * Method hasUpperBound
+     */
+    public boolean hasUpperBound()
+    {
+        return this._has_upperBound;
+    } //-- boolean hasUpperBound() 
+
+    /**
+     * Method isValid
+     */
+    public boolean isValid()
+    {
+        try {
+            validate();
+        }
+        catch (org.exolab.castor.xml.ValidationException vex) {
+            return false;
+        }
+        return true;
+    } //-- boolean isValid() 
+
+    /**
+     * Method marshal
+     * 
+     * @param out
+     */
+    public void marshal(java.io.Writer out)
+        throws org.exolab.castor.xml.MarshalException, org.exolab.castor.xml.ValidationException
+    {
+        
+        Marshaller.marshal(this, out);
+    } //-- void marshal(java.io.Writer) 
+
+    /**
+     * Method marshal
+     * 
+     * @param handler
+     */
+    public void marshal(org.xml.sax.ContentHandler handler)
+        throws java.io.IOException, org.exolab.castor.xml.MarshalException, org.exolab.castor.xml.ValidationException
+    {
+        
+        Marshaller.marshal(this, handler);
+    } //-- void marshal(org.xml.sax.ContentHandler) 
+
+    /**
+     * Sets the value of field 'lowerBound'.
+     * 
+     * @param lowerBound the value of field 'lowerBound'.
+     */
+    public void setLowerBound(double lowerBound)
+    {
+        this._lowerBound = lowerBound;
+        this._has_lowerBound = true;
+    } //-- void setLowerBound(double) 
+
+    /**
+     * Sets the value of field 'startValue'.
+     * 
+     * @param startValue the value of field 'startValue'.
+     */
+    public void setStartValue(double startValue)
+    {
+        this._startValue = startValue;
+        this._has_startValue = true;
+    } //-- void setStartValue(double) 
+
+    /**
+     * Sets the value of field 'stdDev'.
+     * 
+     * @param stdDev the value of field 'stdDev'.
+     */
+    public void setStdDev(double stdDev)
+    {
+        this._stdDev = stdDev;
+        this._has_stdDev = true;
+    } //-- void setStdDev(double) 
+
+    /**
+     * Sets the value of field 'upperBound'.
+     * 
+     * @param upperBound the value of field 'upperBound'.
+     */
+    public void setUpperBound(double upperBound)
+    {
+        this._upperBound = upperBound;
+        this._has_upperBound = true;
+    } //-- void setUpperBound(double) 
+
+    /**
+     * Method unmarshalExternalModelParameterComplexType
+     * 
+     * @param reader
+     */
+    public static org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType unmarshalExternalModelParameterComplexType(java.io.Reader reader)
+        throws org.exolab.castor.xml.MarshalException, org.exolab.castor.xml.ValidationException
+    {
+        return (org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType) Unmarshaller.unmarshal(org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType.class, reader);
+    } //-- org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType unmarshalExternalModelParameterComplexType(java.io.Reader) 
+
+    /**
+     * Method validate
+     */
+    public void validate()
+        throws org.exolab.castor.xml.ValidationException
+    {
+        org.exolab.castor.xml.Validator validator = new org.exolab.castor.xml.Validator();
+        validator.validate(this);
+    } //-- void validate() 
+
+}
---
+++ model_external_socket/castor_temp_src/org/openda/model_external_socket/io/castorgenerated/ExternalSocketModelFactoryConfigXML.java
@@ -0,0 +1,273 @@
+/*
+ * This class was automatically generated with 
+ * <a href="http://www.castor.org">Castor 0.9.4.3</a>, using an XML
+ * Schema.
+ * $Id$
+ */
+
+package org.openda.model_external_socket.io.castorgenerated;
+
+  //---------------------------------/
+ //- Imported classes and packages -/
+//---------------------------------/
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Serializable;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import org.exolab.castor.xml.MarshalException;
+import org.exolab.castor.xml.Marshaller;
+import org.exolab.castor.xml.Unmarshaller;
+import org.exolab.castor.xml.ValidationException;
+import org.xml.sax.ContentHandler;
+
+/**
+ * Class ExternalSocketModelFactoryConfigXML.
+ * 
+ * @version $Revision$ $Date$
+ */
+public class ExternalSocketModelFactoryConfigXML implements java.io.Serializable {
+
+
+      //--------------------------/
+     //- Class/Member Variables -/
+    //--------------------------/
+
+    /**
+     * Field _portNumber
+     */
+    private int _portNumber;
+
+    /**
+     * keeps track of state for field: _portNumber
+     */
+    private boolean _has_portNumber;
+
+    /**
+     * Field _parameterList
+     */
+    private java.util.ArrayList _parameterList;
+
+
+      //----------------/
+     //- Constructors -/
+    //----------------/
+
+    public ExternalSocketModelFactoryConfigXML() {
+        super();
+        _parameterList = new ArrayList();
+    } //-- org.openda.model_external_socket.io.castorgenerated.ExternalSocketModelFactoryConfigXML()
+
+
+      //-----------/
+     //- Methods -/
+    //-----------/
+
+    /**
+     * Method addParameter
+     * 
+     * @param vParameter
+     */
+    public void addParameter(org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType vParameter)
+        throws java.lang.IndexOutOfBoundsException
+    {
+        _parameterList.add(vParameter);
+    } //-- void addParameter(org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType) 
+
+    /**
+     * Method addParameter
+     * 
+     * @param index
+     * @param vParameter
+     */
+    public void addParameter(int index, org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType vParameter)
+        throws java.lang.IndexOutOfBoundsException
+    {
+        _parameterList.add(index, vParameter);
+    } //-- void addParameter(int, org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType) 
+
+    /**
+     * Method clearParameter
+     */
+    public void clearParameter()
+    {
+        _parameterList.clear();
+    } //-- void clearParameter() 
+
+    /**
+     * Method enumerateParameter
+     */
+    public java.util.Enumeration enumerateParameter()
+    {
+        return new org.exolab.castor.util.IteratorEnumeration(_parameterList.iterator());
+    } //-- java.util.Enumeration enumerateParameter() 
+
+    /**
+     * Method getParameter
+     * 
+     * @param index
+     */
+    public org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType getParameter(int index)
+        throws java.lang.IndexOutOfBoundsException
+    {
+        //-- check bounds for index
+        if ((index < 0) || (index > _parameterList.size())) {
+            throw new IndexOutOfBoundsException();
+        }
+        
+        return (org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType) _parameterList.get(index);
+    } //-- org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType getParameter(int) 
+
+    /**
+     * Method getParameter
+     */
+    public org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType[] getParameter()
+    {
+        int size = _parameterList.size();
+        org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType[] mArray = new org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType[size];
+        for (int index = 0; index < size; index++) {
+            mArray[index] = (org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType) _parameterList.get(index);
+        }
+        return mArray;
+    } //-- org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType[] getParameter() 
+
+    /**
+     * Method getParameterCount
+     */
+    public int getParameterCount()
+    {
+        return _parameterList.size();
+    } //-- int getParameterCount() 
+
+    /**
+     * Returns the value of field 'portNumber'.
+     * 
+     * @return the value of field 'portNumber'.
+     */
+    public int getPortNumber()
+    {
+        return this._portNumber;
+    } //-- int getPortNumber() 
+
+    /**
+     * Method hasPortNumber
+     */
+    public boolean hasPortNumber()
+    {
+        return this._has_portNumber;
+    } //-- boolean hasPortNumber() 
+
+    /**
+     * Method isValid
+     */
+    public boolean isValid()
+    {
+        try {
+            validate();
+        }
+        catch (org.exolab.castor.xml.ValidationException vex) {
+            return false;
+        }
+        return true;
+    } //-- boolean isValid() 
+
+    /**
+     * Method marshal
+     * 
+     * @param out
+     */
+    public void marshal(java.io.Writer out)
+        throws org.exolab.castor.xml.MarshalException, org.exolab.castor.xml.ValidationException
+    {
+        
+        Marshaller.marshal(this, out);
+    } //-- void marshal(java.io.Writer) 
+
+    /**
+     * Method marshal
+     * 
+     * @param handler
+     */
+    public void marshal(org.xml.sax.ContentHandler handler)
+        throws java.io.IOException, org.exolab.castor.xml.MarshalException, org.exolab.castor.xml.ValidationException
+    {
+        
+        Marshaller.marshal(this, handler);
+    } //-- void marshal(org.xml.sax.ContentHandler) 
+
+    /**
+     * Method removeParameter
+     * 
+     * @param vParameter
+     */
+    public boolean removeParameter(org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType vParameter)
+    {
+        boolean removed = _parameterList.remove(vParameter);
+        return removed;
+    } //-- boolean removeParameter(org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType) 
+
+    /**
+     * Method setParameter
+     * 
+     * @param index
+     * @param vParameter
+     */
+    public void setParameter(int index, org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType vParameter)
+        throws java.lang.IndexOutOfBoundsException
+    {
+        //-- check bounds for index
+        if ((index < 0) || (index > _parameterList.size())) {
+            throw new IndexOutOfBoundsException();
+        }
+        _parameterList.set(index, vParameter);
+    } //-- void setParameter(int, org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType) 
+
+    /**
+     * Method setParameter
+     * 
+     * @param parameterArray
+     */
+    public void setParameter(org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType[] parameterArray)
+    {
+        //-- copy array
+        _parameterList.clear();
+        for (int i = 0; i < parameterArray.length; i++) {
+            _parameterList.add(parameterArray[i]);
+        }
+    } //-- void setParameter(org.openda.model_external_socket.io.castorgenerated.ExternalModelParameterComplexType) 
+
+    /**
+     * Sets the value of field 'portNumber'.
+     * 
+     * @param portNumber the value of field 'portNumber'.
+     */
+    public void setPortNumber(int portNumber)
+    {
+        this._portNumber = portNumber;
+        this._has_portNumber = true;
+    } //-- void setPortNumber(int) 
+
+    /**
+     * Method unmarshalExternalSocketModelFactoryConfigXML
+     * 
+     * @param reader
+     */
+    public static org.openda.model_external_socket.io.castorgenerated.ExternalSocketModelFactoryConfigXML unmarshalExternalSocketModelFactoryConfigXML(java.io.Reader reader)
+        throws org.exolab.castor.xml.MarshalException, org.exolab.castor.xml.ValidationException
+    {
+        return (org.openda.model_external_socket.io.castorgenerated.ExternalSocketModelFactoryConfigXML) Unmarshaller.unmarshal(org.openda.model_external_socket.io.castorgenerated.ExternalSocketModelFactoryConfigXML.class, reader);
+    } //-- org.openda.model_external_socket.io.castorgenerated.ExternalSocketModelFactoryConfigXML unmarshalExternalSocketModelFactoryConfigXML(java.io.Reader) 
+
+    /**
+     * Method validate
+     */
+    public void validate()
+        throws org.exolab.castor.xml.ValidationException
+    {
+        org.exolab.castor.xml.Validator validator = new org.exolab.castor.xml.Validator();
+        validator.validate(this);
+    } //-- void validate() 
+
+}
