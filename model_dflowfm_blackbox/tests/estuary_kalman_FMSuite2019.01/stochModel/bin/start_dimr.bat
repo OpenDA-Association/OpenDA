39574a8900ee21788da1059d09363149720dbc0d
Stef <stef.hummel@deltares.nl>
2021-08-31 18:11:54 +0200

    Added script to start D-Flow FM.

---
+++ model_dflowfm_blackbox/tests/estuary_kalman_FMSuite2019.01/stochModel/bin/start_dimr.bat
@@ -0,0 +1,30 @@
+@echo off
+set DFLOWFMDIR=
+if "%DFLOWFMDIR%"=="" goto error_not_defined
+
+set dimr="%DFLOWFMDIR%\plugins\DeltaShell.Dimr\kernels\x64\dimr\scripts\run_dimr.bat"
+rem 
+rem check if dimr is available
+rem
+if not exist %dimr% goto error_dimr_not_found
+rem
+rem start DIMR for D-Flow FM 
+rem 
+%dimr% dimrConfigEstuary.xml
+goto eof
+
+rem
+rem error messages
+rem
+
+:error_not_defined
+echo No installation directory of D-Flow FM specified.
+goto eof
+
+:error_dimr_not_found
+echo ERROR in ./stochModel/bin/start_dimr.bat:
+echo File not found: %dimr%
+goto eof
+
+:eof
+exit /B %ERRORLEVEL%
---
+++ model_dflowfm_blackbox/tests/estuary_kalman_FMSuite2019.01_bcfile/stochModel/bin/start_dflowfm.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+#
+module load dflowfm
+run_dflowfm.sh --autostartstop $1
