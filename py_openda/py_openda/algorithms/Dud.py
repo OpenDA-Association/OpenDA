0871bd21eaf18d2f9d2cc9ee87b44ea02c756b63
Nils van Velzen <nilsvanvelzen@gmail.com>
2021-09-24 13:55:05 +0200

    Improvements to Python DUD algorithm including reinitialization in case of stagnation

--- py_openda/py_openda/algorithms/Dud.py
+++ py_openda/py_openda/algorithms/Dud.py
@@ -21,6 +21,7 @@ def check_A(A):
     """
     bad = False
     cnum = np.linalg.cond(A)
+    print("Condition number is:"+str(cnum))
     if np.linalg.cond(A) > 1e20:
         print("WARNING: gradient is approximately zero. Iteration is stopped.")
         print("A="+str(A))
@@ -28,7 +29,7 @@ def check_A(A):
     return bad
 
 
-def initialize_dud(func, p_old, obs, std, p_pert=None, start_dist=1.1, start_eps=0.1):
+def initialize_dud(func, p_old, obs, std, p_pert=None, l_bound=None, u_bound=None, start_dist=1.1, start_eps=0.1):
     """
     Function used to find the initial search directions.
 
@@ -46,12 +47,23 @@ def initialize_dud(func, p_old, obs, std, p_pert=None, start_dist=1.1, start_eps
     plist += func(plist)
     plist.append(sum(0.5*((y-x)/z)**2 for y, x, z in zip(obs, plist[p_number:], std)))
     params = np.transpose(np.array([plist]))
+    # Setup the initial pertubations
+    # Make sure we do not perturb outside the variable bounds
     for i in range(p_number):
         plist = p_old.copy()
         if p_pert:
-            plist[i] = plist[i]+p_pert[i]
+            p_step = p_pert[i]
         else:
-            plist[i] = plist[i]*start_dist+start_eps
+            p_step = plist[i] * (start_dist-1.0) + start_eps
+        #Check boundaries and try to fix issues by swapping direction
+        if not (l_bound[i] < plist[i]+p_step < u_bound[i]):
+            if not (l_bound[i] < plist[i]-p_step < u_bound[i]):
+                raise ValueError("initial pertubation is too large it extends boundaries in both directions")
+            else:
+                plist[i] = plist[i] - p_step
+        else:
+            plist[i] = plist[i] + p_step
+
         plist += func(plist)
         plist.append(sum(0.5*((y-x)/z)**2 for y, x, z in zip(obs, plist[p_number:], std)))
         params = np.c_[params, np.transpose(np.array([plist]))]
@@ -104,10 +116,8 @@ def check_step_conv(p_new, parameters, p_tol=1.0e-4):
 
 
 
+def max_step_p_new(parameters, p_new, l_bound, u_bound, alpha_min=1e-2):
 
-
-
-def max_step_p_new(parameters, p_new, l_bound, u_bound):
     # Check for valid starting point
     if not all([l <= p <= u for p, l, u in zip(parameters[:, -1], l_bound, u_bound)]):
         print("print all parameters and bound, error will follow")
@@ -115,22 +125,41 @@ def max_step_p_new(parameters, p_new, l_bound, u_bound):
         for p, l, u in zip(parameters[:, -1], l_bound, u_bound):
             print (str(l)+" "+str(p) + " "+str(u))
         raise ValueError("current parameters are outside upper and lower bounds")
+
+    # Compute max stepsize (within alpha_min)
     alpha = 1.0
-    for p, p_n, l, u in zip (parameters[:, -1], p_new, l_bound, u_bound):
+    # Compute the maximum relative stepsize
+    alpha_max=[]
+    bound_reset_needed=False
+    for p, p_n, l, u, in zip(parameters[:, -1], p_new, l_bound, u_bound):
         if p_n < l:
-            alpha_max_i = (p-l)/(p-p_n)
+            alpha_max_i = (p - l) / (p - p_n)
         elif p_n > u:
-            alpha_max_i = (u-p)/(p_n-p)
+            alpha_max_i = (u - p) / (p_n - p)
         else:
             alpha_max_i = 1.0
+        if alpha_max_i < alpha_min:
+            alpha_max_i = 1.0
+            bound_reset_needed = True
         alpha = min(alpha, alpha_max_i)
 
-    if alpha < 1.0:
-        print("domain protection alpha ="+str(alpha))
-        p_bound = [p + alpha*0.99*(p_n-p) for p, p_n in zip(parameters[:,-1], p_new)]
-        return np.array(p_bound)
-    else:
-        return p_new
+    if (bound_reset_needed):
+        print("Warning: bound reset needed, adjusting search direction")
+    p_corrected =[]
+    if alpha<1.0: alpha=alpha*0.99
+
+    i = 0
+    for p, p_n, l, u, in zip(parameters[:, -1], p_new, l_bound, u_bound):
+        p_corrected_i = p + alpha * (p_n - p)
+        if p_corrected_i < l:
+            print("Parameter: "+str(i)+" hits lower boundary "+str(l))
+            p_corrected_i = 0.5*(p+l)   #Half distance between boundary and current value
+        elif p_corrected_i > u:
+            print("Parameter: "+str(i)+" hits upper boundary "+str(u))
+            p_corrected_i = 0.5*(u+p)    #Half distance between boundary and current value
+        p_corrected.append(p_corrected_i)
+        i+=1
+    return np.array(p_corrected)
 
 
 
@@ -150,29 +179,30 @@ def line_search(func, parameters, func_evals, total_cost, obs, std, p_new):
     print("Linesearch: Cost to reduce:"+str(math.sqrt(2.0* total_cost[-1])))
     p_step = p_new-parameters[:, -1]
     p_rel = [abs(dp)/max(abs(p),1.0e-8) for dp, p in zip(p_step, parameters[:, -1])]
-    print("initial step is"+str(p_new-parameters[:, -1]))
-    print("relative stepsize ="+str(p_rel))
+    print("search-direction"+str(p_new-parameters[:, -1]))
+    #print("relative stepsize ="+str(p_rel))
 
     next_parameters = p_new
     next_func_evals = func(next_parameters)
     next_total_cost = sum(0.5*((y-x)/z)**2 for y, x, z in zip(obs, next_func_evals, std))
     d = 1
     print("alpha="+str(d)+" cost="+str(math.sqrt(2.0*next_total_cost)))
+
     while next_total_cost > total_cost[-1]:
         d *= 0.5
         next_parameters = d*p_new+(1-d)*parameters[:, -1]
         next_func_evals = func(next_parameters)
         next_total_cost = sum(0.5*((y-x)/z)**2 for y, x, z in zip(obs, next_func_evals, std))
         print("alpha=" + str(d) + " cost=" + str(math.sqrt(2.0* next_total_cost)))
+        # Step becomes too small stop searcing
         if abs(d) < 0.0625:
-            if d < 0:
-                break
-            print("Warning swapping search direction")
-            d *= -1
-    return (next_parameters, next_func_evals, next_total_cost)
+            break
+    succes =  next_total_cost < total_cost[-1]
+
+    return (next_parameters, next_func_evals, next_total_cost, succes)
 
 
-def dud(func, p_old, p_pert, obs, std, xtol=1e-3, start_dist=1.1, l_bound=None, u_bound=None, max_iter=10):
+def dud(func, p_old, p_pert, obs, std, xtol=1e-3, start_dist=1.1, l_bound=None, u_bound=None, max_iter=10, max_restart=1):
     """
     Main function which minimizes a least squares problem without using derivatives.
 
@@ -182,6 +212,11 @@ def dud(func, p_old, p_pert, obs, std, xtol=1e-3, start_dist=1.1, l_bound=None, 
     :param std: list of corresponding standard deviations.
     :param xtol: desired accuracy of the result (default 1e-3).
     :param start_dist: Factor by which p_old is multiplied to find
+    :param l_bound: lower bound on variables  (Note: DUD uses bounds in linesearch to avoid object function evaluation
+                                                    with illegal values there is no robust handing of constraints)
+    :param u_bound: upper bound on variables
+    :param max_iter: max number of DUD (outer) iterations
+    :max_restart: max number of times DUD reinitialises when no improvement can be found (re-perturbing all parameters)
     the search directions (default 1.1).
     :return: tuple containing the minimal cost followed by the list of corresponding parameters.
     """
@@ -193,10 +228,22 @@ def dud(func, p_old, p_pert, obs, std, xtol=1e-3, start_dist=1.1, l_bound=None, 
     finish = 0
     max_step = 10
     p_number = len(p_old)
-    (parameters, func_evals, total_cost) = initialize_dud(func, p_old, obs, std, p_pert=p_pert, start_dist=start_dist,
+    (parameters, func_evals, total_cost) = initialize_dud(func, p_old, obs, std, p_pert=p_pert,
+                                                          l_bound=l_bound, u_bound=u_bound, start_dist=start_dist,
                                                           start_eps=0.1)
+    hist = {}
+    hist["parameters"] = list(parameters.copy())
+    hist["func_evals"] = list(func_evals.copy())
+    hist["total_cost"] = list(total_cost.copy())
     iter =0
+
+    restart_cycles = 0
+    reinitialize = False
+
     while True:
+
+
+
         (stop, p_new) = find_next_params(p_number, parameters, func_evals, obs, std, max_step)
         if stop:
             break
@@ -206,28 +253,56 @@ def dud(func, p_old, p_pert, obs, std, xtol=1e-3, start_dist=1.1, l_bound=None, 
             break
 
         p_new = max_step_p_new(parameters, p_new, l_bound, u_bound)
-        (next_parameters, next_func_evals, next_total_cost) = line_search(func, parameters,
+        (next_parameters, next_func_evals, next_total_cost, success) = line_search(func, parameters,
                                                                           func_evals, total_cost,
                                                                           obs, std, p_new)
-        next_params = np.concatenate((next_parameters,
-                                      np.array(next_func_evals),
-                                      np.array([next_total_cost])))
-        all_params = np.concatenate((parameters, func_evals, np.expand_dims(total_cost, 0)))
-        all_params = np.delete(all_params, 0, 1)
-        all_params = np.c_[all_params, next_params]
-        all_params = all_params[:, np.argsort(-all_params[-1, :])]
-        parameters = all_params[:p_number, :]
-        func_evals = all_params[p_number:-1, :]
-        total_cost = all_params[-1, :]
-        if abs(total_cost[-1] - total_cost[-2]) < xtol * abs(total_cost[-2]):
-            finish += 1
-        else:
-            finish = 0
-        if finish > 2:
-            break
-        iter+=1
-        if iter>=max_iter:
-            print("terminated in max number of iterations")
-            break
 
-    return (total_cost[-1], parameters[:, -1])
+        if not success and restart_cycles<max_restart:
+            reinitialize=True
+            restart_cycles+=1
+        if reinitialize:
+            print("DUD: Reinitialize linearization")
+            #Linear approximation can become quite poor.
+            p_pert_restart = [p*0.1 for p in p_pert]
+            (parameters, func_evals, total_cost) = initialize_dud(func, list(next_parameters), obs, std, p_pert=p_pert_restart,
+                                                                  l_bound=l_bound, u_bound=u_bound,
+                                                                  start_dist=start_dist,
+                                                                  start_eps=0.1)
+            reinitialize = False
+        else:
+            next_params = np.concatenate((next_parameters,
+                                          np.array(next_func_evals),
+                                          np.array([next_total_cost])))
+
+            hist["parameters"].append(next_parameters.copy())
+            hist["func_evals"].append(next_func_evals.copy())
+            hist["total_cost"].append(next_total_cost.copy())
+
+
+            all_params = np.concatenate((parameters, func_evals, np.expand_dims(total_cost, 0)))
+            all_params = np.delete(all_params, 0, 1)
+            all_params = np.c_[all_params, next_params]
+            all_params = all_params[:, np.argsort(-all_params[-1, :])]
+            parameters = all_params[:p_number, :]
+            func_evals = all_params[p_number:-1, :]
+            total_cost = all_params[-1, :]
+
+
+            if abs(total_cost[-1] - total_cost[-2]) < xtol * abs(total_cost[-2]):
+                finish += 1
+            elif:
+
+            else:
+                finish = 0
+            if finish > 2:
+                break
+            iter+=1
+            if iter>=max_iter:
+                print("terminated in max number of iterations")
+                break
+
+    #tidy up hist
+    hist["parameters"] = np.array([list(p) for p in hist["parameters"]]).T.tolist()
+    hist["func_evals"] = np.array([list(p) for p in hist["func_evals"]]).T.tolist()
+
+    return (total_cost[-1], parameters[:, -1], hist)
--- py_openda/py_openda/algorithms/Dud_test.py
+++ py_openda/py_openda/algorithms/Dud_test.py
@@ -7,20 +7,20 @@ Created on Fri Oct 12 11:06:08 2018
 """
 
 import unittest
-import Dud
+from py_openda.algorithms import Dud
 
 def f(p):
     return [p[0], p[1]]
 
-a= Dud.dud(f, [2, 7], [1, 33], [1,1])
-
 class TestDud(unittest.TestCase):
     
     def test_dud(self):
+        (total_cost, p_opt, hist) = Dud.dud(f, p_old=[2, 7], p_pert=[0.1, 0.1], obs=[1, 33], std=[1.0, 1.0])
 
-        self.assertAlmostEqual(a[0], 0)
-        self.assertAlmostEqual(a[1][0],1)
-        self.assertAlmostEqual(a[1][1],33)
+
+        self.assertAlmostEqual(total_cost, 0.0)
+        self.assertAlmostEqual(p_opt[0], 1.0)
+        self.assertAlmostEqual(p_opt[1], 33.0)
 
 
 if __name__ == '__main__':
