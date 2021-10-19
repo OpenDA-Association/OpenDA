2dcc4e9a82ef20c4e380299308be50adf043dbe7
sumihar <julius.sumihar@deltares.nl>
2021-10-19 15:53:37 +0200

    use start_dimr.sh and start_dimr.bat in all dflowfm examples in directory tests
    All examples in directory tests make now use of only start_dimr.sh/.bat. The start_dflowfm* are removed. The scripts contain an empty variable DFLOWFMDIR. To use the scripts, user needs to specify it. The scripts contain error message when dimr is not found.
    
    Specific for test example calibration_discharge_dependent_river_roughness: fix issue with river1D.crs (it's replaced by river1D_crs.pli; the river1D.mdu is adjusted accordingly).

--- model_dflowfm_blackbox/tests/dcsmv5_kalman/stochModel/bin/start_dimr.bat
+++ model_dflowfm_blackbox/tests/dcsmv5_kalman/stochModel/bin/start_dimr.bat
@@ -1,16 +1,21 @@
 @echo off
+rem This script requires one input argument i.e. file name of the dimr_config.xml
+rem To use this script, set DFLOWFMDIR to the installation directory of D-Flow FM.
 set DFLOWFMDIR=
+rem set DFLOWFMDIR=p:\h6\opt\delft3dfm\2.13.02.67836\
 if "%DFLOWFMDIR%"=="" goto error_not_defined
 
-set dimr="%DFLOWFMDIR%\plugins\DeltaShell.Dimr\kernels\x64\dimr\scripts\run_dimr.bat"
+set dimr="%DFLOWFMDIR%\x64\dimr\scripts\run_dimr.bat"
+rem set dimr="%DFLOWFMDIR%\plugins\DeltaShell.Dimr\run_dimr.bat"
 rem 
 rem check if dimr is available
 rem
 if not exist %dimr% goto error_dimr_not_found
+
 rem
 rem start DIMR for D-Flow FM 
 rem 
-%dimr% dimrConfig.xml
+%dimr% %1
 goto eof
 
 rem
@@ -18,6 +23,7 @@ rem error messages
 rem
 
 :error_not_defined
+echo ERROR in ./stochModel/bin/start_dimr.bat:
 echo No installation directory of D-Flow FM specified.
 goto eof
 
