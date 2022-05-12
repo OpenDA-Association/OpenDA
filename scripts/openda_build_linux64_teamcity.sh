a5362e68327715d96b100938029ba4015eb4d292
Alja Vrieling <alja.vrieling@vortech.nl>
2022-05-12 11:32:10 +0200

    issue ant overwriting JAVA_HOME on TeamCity

--- scripts/openda_build_linux64_teamcity.sh
+++ scripts/openda_build_linux64_teamcity.sh
@@ -13,9 +13,13 @@ rm -rf public/*.tgz
 # load dev dependencies for native
 export GCCDIR=/opt/apps/gcc/9.2.0
 module load gcc/9.2.0
+module load ant/1.10.9
 module load automake/1.16.2_gcc9.2.0
 module load autoconf/2.69_gcc9.2.0
 module load libtool/2.4.6_gcc9.2.0
+# overwrite JAVA_HOME set by module load ant
+# reason: incorrect installation of ant/java on TeamCity agents
+export JAVA_HOME=/opt/apps/java/amazon-corretto-11.0.7.10.1-linux-x64/amazon-corretto-11.0.7.10.1-linux-x64
 
 # build native
 public/scripts/openda_build_linux64_gnu.sh
@@ -30,6 +34,5 @@ cp $GCCDIR/lib64/libgfortran.so.5 $LIB64
 cp $GCCDIR/lib64/libquadmath.so.0 $LIB64
 
 # create tgz
-module load ant/1.10.9
 cd public
 ant tgz-native
