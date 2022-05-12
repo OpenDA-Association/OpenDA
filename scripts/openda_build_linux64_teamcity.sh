d180df0e606ce24f7ce2d9c645ea12a867bf9e7b
Alja Vrieling <alja.vrieling@vortech.nl>
2022-05-12 11:32:10 +0200

    issue ant overwriting JAVA_HOME on TeamCity

--- scripts/openda_build_linux64_teamcity.sh
+++ scripts/openda_build_linux64_teamcity.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 module(){ eval $(/usr/bin/modulecmd bash $*);};
- 
+
 export OPENDASCRIPTROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${OPENDASCRIPTROOT}/../..
 
@@ -11,8 +11,12 @@ module load ant/1.10.9
 module load automake/1.16.2_gcc9.2.0
 module load autoconf/2.69_gcc9.2.0
 module load libtool/2.4.6_gcc9.2.0
-rm -rf public/bin/*
-rm -rf public/*.tgz
+
+# overwrite JAVA_HOME set by module load ant
+# reason: incorrect installation of ant/java on TeamCity agents
+export JAVA_HOME=/opt/apps/java/amazon-corretto-11.0.7.10.1-linux-x64/amazon-corretto-11.0.7.10.1-linux-x64
+
+# build native
 public/scripts/openda_build_linux64_gnu.sh
 if [ $? -gt 0 ]; then
         echo "error while building openda native"
@@ -21,5 +25,7 @@ fi
 export LIB64=public/core/native_bin/linux64_gnu/lib
 cp $GCCDIR/lib64/libgfortran.so.5 $LIB64
 cp $GCCDIR/lib64/libquadmath.so.0 $LIB64
+
+# create tgz
 cd public
 ant tgz-native
