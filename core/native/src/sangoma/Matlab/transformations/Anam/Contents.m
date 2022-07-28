ee725fabcceaf850b0e9bf29b196a4aa6edc4459
Alja Vrieling <alja.vrieling@vortech.nl>
2022-07-28 09:28:43 +0200

    Remove sangoma source code.

--- core/native/src/sangoma/Matlab/transformations/Anam/Contents.m
+++
@@ -1,7 +0,0 @@
-% Empirical Gaussian Anamorphosis (Anam)
-% This package determines the empirical transformation function such that a transformed variable should follow a Gaussian distribution.
-% version 1.0 13-Jul-2010
-%
-% anam_setup        - determine the empirical transformation function
-% anam_transform    - transform the data according to the transformation anam
-% anam_invtransform - apply inverse transformation according to the transformation anam
