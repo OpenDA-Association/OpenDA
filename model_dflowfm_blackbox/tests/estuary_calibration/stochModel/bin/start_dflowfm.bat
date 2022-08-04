f2beea1a00963a76faa35baf8054320d51aee7bb
Alja Vrieling <alja.vrieling@vortech.nl>
2022-08-04 17:18:51 +0200

    removed start_dflowfm.* scripts from D-Flow FM examples

--- model_dflowfm_blackbox/tests/estuary_calibration/stochModel/bin/start_dflowfm.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-rem Please adjust path below to local FM installation dir
-"c:\Program Files (x86)\Deltares\Delft3D FM Suite 2020.02 HMWQ (1.6.1.47098)\plugins\DeltaShell.Dimr\kernels\x64\dflowfm\scripts\run_dflowfm.bat" estuary.mdu
