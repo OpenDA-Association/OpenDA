401e20185556c029aec1563f7f53709ee8e9aabb
Alja Vrieling <alja.vrieling@vortech.nl>
2022-09-29 16:49:33 +0200

    Repair of start_dimr.sh due to incomplete merge

--- model_dflowfm_blackbox/tests/lake_kalman/stochModel/bin/start_dimr.sh
+++ model_dflowfm_blackbox/tests/lake_kalman/stochModel/bin/start_dimr.sh
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-## This script requires one input argument i.e. file name of the dimr_config.xml
-## To use this script, set DFLOWFMDIR to the installation directory of DIMR
-export DFLOWFMDIR=
-#export DFLOWFMDIR=/opt/delft3dfm/2.13.02.67836
-=======
 # This script requires one input argument: a file name (typically called dimr_config.xml). 
 # To use this script, uncomment the next line and set DFLOWFMDIR to <Your installation base dir>
 #export DFLOWFMDIR=
 # or create an environment variable DFLOWFMDIR.
->>>>>>> 8ebf8ca27... Added start_dimr.sh scripts to D-FLOW FM examples
 if [ -z $1 ]; then
     echo ERROR: no dimr configuration file is specified
     exit 1
