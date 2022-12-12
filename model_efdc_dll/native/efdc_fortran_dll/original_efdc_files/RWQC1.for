caf4e91b6f3c5c90c3df9333e6ca80a08effc812
Werner Kramer <werner.kramer@vortech.nl>
2022-12-12 16:32:00 +0100

    Disable SEEK('C21') call as in provided source

--- model_efdc_dll/native/efdc_fortran_dll/original_efdc_files/RWQC1.for
+++ model_efdc_dll/native/efdc_fortran_dll/original_efdc_files/RWQC1.for
@@ -2082,7 +2082,7 @@ C ***     MG/L FOR 1-19, TAM-MOLES/L, AND FCB-MPN/L
           ENDDO          
         ENDIF
 ! *** C21 WQ3DWC2.INP
-        CALL SEEK('C21')  
+!        CALL SEEK('C21')  
         IF(ISSKIP .GT. 0) CALL SKIPCOMM(1,CCMRM)  
         IF(ISSKIP .EQ. 0) READ(1,*)  
         write(2,*) 'C21'
