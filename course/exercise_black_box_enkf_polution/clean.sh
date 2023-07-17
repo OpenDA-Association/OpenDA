c33909b544832ac479bfec486bc7286b52f30dca
robot144 <robot144@protonmail.com>
2023-07-17 18:30:14 +0200

    add/update/cleanup scripts for course

---
+++ course/exercise_black_box_enkf_polution/clean.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+rm -f original_model/maps/*.txt
+rm -f original_model/restart/*.txt
+rm -f original_model/c*_loc*.csv
+rm -f original_model/reactive_pollution_model.log 
+rm -f [0-9][0-9][0-9][0-9]
+rm -rf work/work*
+rm -f *_results.py
+rm -f openda_logfile.txt
+rm -rf __pycache__
+rm -f twinTrue/c[1-2]_loc[A-C].csv
+rm -rf twinTrue/maps
+rm -rf twinTrue/restart
