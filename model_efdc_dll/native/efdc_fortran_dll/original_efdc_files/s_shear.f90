bf26cad9affe54637c575a691d7b8c27e4f802c1
Max van der Kolk <max.vanderkolk@vortech.nl>
2023-11-30 16:46:24 +0100

    Resolve compiler warnings due to `Wconversion`
    All implicit conversion is made explicit by inserting the required type
    casts to match the assigned variables.

--- model_efdc_dll/native/efdc_fortran_dll/original_efdc_files/s_shear.f90
+++ model_efdc_dll/native/efdc_fortran_dll/original_efdc_files/s_shear.f90
@@ -212,7 +212,7 @@ SUBROUTINE SEDZLJ_SHEAR
              FWW(L)=MMW*0.15/JJW
              !Calculate total wave and current shear stress (dynes/cm^2)
              TAU(L)=0.5*FWW(L)*WVORBIT(L)**2*10000.0*MMW
-             TAUB(L)=0.1*TAU(L) !PT: conversion from dynes/cm^2 to Pa
+             TAUB(L)=REAL(0.1*TAU(L),KIND(TAUB)) !PT: conversion from dynes/cm^2 to Pa
              USTAR(L)=SQRT(TAUB(L)/1000.0) !only true because USTAR=SQRT(Tau/RhoH2O) and RhoH2O is 1000 kg/cm^3. 
           ENDIF
         ENDIF
@@ -222,7 +222,7 @@ SUBROUTINE SEDZLJ_SHEAR
   ELSE !Set constant tau is TAUCONST (dynes/cm^2) is greater than 0
      DO L=2,LA
         TAU(L)=TAUCONST
-        TAUB(L)=0.1*TAU(L)
+        TAUB(L)=REAL(0.1*TAU(L),KIND(TAUB))
         USTAR(L)=SQRT(TAUB(L)/1000.)
      ENDDO
   ENDIF
