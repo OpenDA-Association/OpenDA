28876eb5999e05b1aee83ff871acde4639a678c7
Cees Voesenek <cees.voesenek@vortech.nl>
2024-04-02 15:08:42 +0200

    Tweak test script
    - Reduce repetition from the test script;
    - Removes logs a bit less heavy-handedly;
    - Return a non-zero exit code when tests fail;
    - Add a --teamcity flag to mark tests in TeamCity.

--- core/tests/run_all_tests.sh
+++ core/tests/run_all_tests.sh
@@ -1,20 +1,54 @@
-#! /bin/sh
-set -x 
+#!/bin/bash
 
-./clean.sh
-rm -f *.log
+./clean.sh &> /dev/null
 
-oda_test.sh simple_lorenz >simple_lorenz.log 2>&1
-oda_test.sh simple_lorenz96 >simple_lorenz96.log 2>&1
-oda_test.sh simple_lorenz_transformed_observations >simple_lorenz_transformed_observations.log 2>&1
-oda_test.sh simple_oscillator >simple_oscillator.log 2>&1
-oda_test.sh simple_resultwriters >simple_resultwriters.log 2>&1
-oda_test.sh simple_river1d >simple_river1d.log 2>&1
-oda_test.sh simple_two_oscillators >simple_two_oscillators.log 2>&1
-oda_test.sh native_advec1d >native_advec1d.log 2>&1
-oda_test.sh native_heat >native_heat.log 2>&1
-oda_test.sh native_oscillator >native_oscillator.log 2>&1
-oda_test.sh native_parallel >native_parallel.log 2>&1
-oda_test.sh native_parallel_from_examples >native_parallel_from_examples.log 2>&1
-oda_test.sh thread >thread.log 2>&1
-oda_test.sh rmi >rmi.log 2>&1
+is_teamcity=false
+if [ "$1" == "--teamcity" ]; then
+    is_teamcity=true
+fi
+
+tests=(
+    'simple_lorenz'
+    'simple_lorenz96'
+    'simple_lorenz_transformed_observations'
+    'simple_oscillator'
+    'simple_resultwriters'
+    'simple_river1d'
+    'simple_two_oscillators'
+    'native_advec1d'
+    'native_heat'
+    'native_oscillator'
+    'native_parallel'
+    'native_parallel_from_examples'
+    'thread'
+    'rmi'
+)
+
+is_error=0
+for test in "${tests[@]}"; do
+    # Remove log from previous test.
+    rm -f "${test}.log"
+
+    if [ $is_teamcity = true ]; then
+        echo "##teamcity[testStarted name='${test}' captureStandardOutput='true']"
+        if oda_test.sh "${test}"; then
+            echo "##teamcity[testFinished name='${test}']"
+        else
+            is_error=1
+            echo "##teamcity[testFailed name='${test}' message='{$test} failed' details='']"
+        fi
+    else
+        echo -n "Running test $test..."
+        if oda_test.sh "${test}" &> "${test}.log"; then
+            echo "passed"
+        else
+            is_error=1
+            echo "failed"
+        fi
+    fi
+done
+
+# If any test has failed, exit with error code.
+if [ $is_error -ne 0 ]; then
+    exit 1
+fi
