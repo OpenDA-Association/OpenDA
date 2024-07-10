2edb4353ed19608de4207744bbc56a4979a307c3
michael topsom <michael.topsom@deltares.nl>
2024-07-10 15:44:50 +0200

    #377 Updated ini file exchange item to read all parameters and store ini file in finish method

--- model_wanda_seawat/java/src/org/openda/model_wanda_seawat/WandaSeawatModelRunIniFileDataObject.java
+++ model_wanda_seawat/java/src/org/openda/model_wanda_seawat/WandaSeawatModelRunIniFileDataObject.java
@@ -1,60 +1,72 @@
 package org.openda.model_wanda_seawat;
 
 import org.ini4j.Ini;
+import org.ini4j.Profile;
 import org.openda.exchange.AbstractDataObject;
 import org.openda.exchange.DoubleExchangeItem;
-import org.openda.exchange.IntExchangeItem;
-import org.openda.exchange.StringExchangeItem;
+import org.openda.exchange.timeseries.TimeUtils;
 import org.openda.interfaces.IExchangeItem;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
 
 public class WandaSeawatModelRunIniFileDataObject extends AbstractDataObject {
 	private static final String SECTION_RUN = "run";
 	private static final String OPTION_START_DATE_TIME = "startDateTime";
 	private static final String OPTION_END_DATE_TIME = "endDateTime";
 	private static final String SECTION_PARAMETERS = "parameters";
-	private static final String OPTION_BULK_DENSITY = "HTGEOFIL H1.Geoformations.Bulk density formation.kleilaag 1";
-	private static final String OPTION_POROSITY = "HTGEOFIL H1.Geoformations.Porosity formation.kleilaag 1";
-	private static final String OPTION_MAX_YEARLY_WATER_VOLUME = "HTGEOFIL H1.Max yearly water volume";
+	private File file;
+	private Ini ini;
 
 	@Override
 	public void finish() {
-		// Do nothing
+		if (exchangeItems.isEmpty()) {
+			throw new RuntimeException("Exchange items not yet initialized.");
+		}
+		DoubleExchangeItem startTimeExchangeItem = (DoubleExchangeItem) exchangeItems.get(OPTION_START_DATE_TIME);
+		ini.put(SECTION_RUN, OPTION_START_DATE_TIME, startTimeExchangeItem.getValue());
+		DoubleExchangeItem endTimeExchangeItem = (DoubleExchangeItem) exchangeItems.get(OPTION_END_DATE_TIME);
+		ini.put(SECTION_RUN, OPTION_END_DATE_TIME, endTimeExchangeItem.getValue());
+		Profile.Section optionsFromParameters = ini.get(SECTION_PARAMETERS);
+		for (String option : optionsFromParameters.keySet()) {
+			DoubleExchangeItem parameterExchangeItem = (DoubleExchangeItem) exchangeItems.get(option); 
+			ini.put(SECTION_PARAMETERS, option, parameterExchangeItem.getValue());
+		}
+		try {
+			ini.store(file);
+		} catch (IOException exception) {
+			throw new RuntimeException(exception);
+		}
 	}
 
 	@Override
 	public void initialize(File workingDir, String[] arguments) {
-		File file = new File(workingDir, arguments[0]);
-		Ini ini = new Ini();
+		file = new File(workingDir, arguments[0]);
+		ini = new Ini();
 		try {
 			ini.load(file);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
+			dateTimeExchangeItem(ini, OPTION_START_DATE_TIME, OPTION_START_DATE_TIME);
+			dateTimeExchangeItem(ini, OPTION_END_DATE_TIME, OPTION_END_DATE_TIME);
+		} catch (IOException | ParseException exception) {
+			throw new RuntimeException(exception);
 		}
-		dateTimeExchangeItem(ini, OPTION_START_DATE_TIME, "startDateTime");
-		dateTimeExchangeItem(ini, OPTION_END_DATE_TIME, "endDateTime");
-		doubleExchangeItem(ini, OPTION_BULK_DENSITY, "h1GeoformationsBulkDensity");
-		doubleExchangeItem(ini, OPTION_POROSITY, "h1GeoformationsPorosity");
-		intExchangeItem(ini, OPTION_MAX_YEARLY_WATER_VOLUME, "h1MaxYearlyWaterVolume");
+		Profile.Section optionsFromParameters = ini.get(SECTION_PARAMETERS);
+		for (String option : optionsFromParameters.keySet()) {
+			doubleExchangeItem(ini, option, option);
+		}
 	}
 
-	private void dateTimeExchangeItem(Ini ini, String optionName, String id) {
+	private void dateTimeExchangeItem(Ini ini, String optionName, String id) throws ParseException {
 		String valueString = ini.get(SECTION_RUN, optionName);
-		StringExchangeItem stringExchangeItem = new StringExchangeItem(id, IExchangeItem.Role.InOut, valueString);
-		exchangeItems.putIfAbsent(id, stringExchangeItem);
+		double dateTime = TimeUtils.date2Mjd(valueString);
+		DoubleExchangeItem doubleExchangeItem = new DoubleExchangeItem(id, IExchangeItem.Role.InOut, dateTime);
+		exchangeItems.putIfAbsent(id, doubleExchangeItem);
 	}
 	
 	private void doubleExchangeItem(Ini ini, String optionName, String id) {
 		String valueString = ini.get(SECTION_PARAMETERS, optionName);
 		DoubleExchangeItem doubleExchangeItem = new DoubleExchangeItem(id, IExchangeItem.Role.InOut, Double.parseDouble(valueString));
 		exchangeItems.putIfAbsent(id, doubleExchangeItem);
-	}
-
-	private void intExchangeItem(Ini ini, String optionName, String id) {
-		String valueString = ini.get(SECTION_PARAMETERS, optionName);
-		IntExchangeItem intExchangeItem = new IntExchangeItem(id, IExchangeItem.Role.InOut, Integer.parseInt(valueString));
-		exchangeItems.putIfAbsent(id, intExchangeItem);
 	}
 }
--- model_wanda_seawat/java/test/org/openda/model_wanda_seawat/WandaSeawatModelRunIniFileDataObjectTest.java
+++ model_wanda_seawat/java/test/org/openda/model_wanda_seawat/WandaSeawatModelRunIniFileDataObjectTest.java
@@ -7,6 +7,11 @@ import org.openda.exchange.StringExchangeItem;
 import org.openda.utils.OpenDaTestSupport;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class WandaSeawatModelRunIniFileDataObjectTest extends TestCase {
 	OpenDaTestSupport testData = null;
@@ -17,35 +22,43 @@ public class WandaSeawatModelRunIniFileDataObjectTest extends TestCase {
 		testRunDataDir = new File(testData.getTestRunDataDir(), "WandaSeawatModelRunIniFileDataObject");
 	}
 
-	public void test_givenWandaSeawatIniFile_whenInitialized_thenExchangeItemsLoaded() {
+	public void test_givenWandaSeawatIniFile_whenInitialized_thenExchangeItemsLoaded() throws IOException {
 		File file = new File(testRunDataDir, "example.ini");
 		WandaSeawatModelRunIniFileDataObject wandaSeawatModelRunIniFileDataObject = new WandaSeawatModelRunIniFileDataObject();
 		wandaSeawatModelRunIniFileDataObject.initialize(testRunDataDir, new String[]{file.getName()});
 		assertEquals("The 5 exchange items exist", 5, wandaSeawatModelRunIniFileDataObject.getExchangeItemIDs().length);
 
 		Object startDateTimeExchangeItemObject = wandaSeawatModelRunIniFileDataObject.getDataObjectExchangeItem("startDateTime");
-		assertTrue(startDateTimeExchangeItemObject instanceof StringExchangeItem);
-		StringExchangeItem startDateTimeExchangeItem = (StringExchangeItem) startDateTimeExchangeItemObject;
-		assertEquals("The start date time is 20240101000000", "20240101000000", startDateTimeExchangeItem.getValue());
+		assertTrue(startDateTimeExchangeItemObject instanceof DoubleExchangeItem);
+		DoubleExchangeItem startDateTimeExchangeItem = (DoubleExchangeItem) startDateTimeExchangeItemObject;
+		assertEquals("The start date time (20240101000000) is 60310.0", 60310.0, startDateTimeExchangeItem.getValue());
 
 		Object endDateTimeExchangeItemObject = wandaSeawatModelRunIniFileDataObject.getDataObjectExchangeItem("endDateTime");
-		assertTrue(endDateTimeExchangeItemObject instanceof StringExchangeItem);
-		StringExchangeItem endDateTimeExchangeItem = (StringExchangeItem) endDateTimeExchangeItemObject;
-		assertEquals("The end date time is 20240102000000", "20240102000000", endDateTimeExchangeItem.getValue());
+		assertTrue(endDateTimeExchangeItemObject instanceof DoubleExchangeItem);
+		DoubleExchangeItem endDateTimeExchangeItem = (DoubleExchangeItem) endDateTimeExchangeItemObject;
+		assertEquals("The end date time (20240102000000) is 60311.0", 60311.0, endDateTimeExchangeItem.getValue());
 
-		Object bulkDensityExchangeItemObject = wandaSeawatModelRunIniFileDataObject.getDataObjectExchangeItem("h1GeoformationsBulkDensity");
+		Object bulkDensityExchangeItemObject = wandaSeawatModelRunIniFileDataObject.getDataObjectExchangeItem("HTGEOFIL H1.Geoformations.Bulk density formation.kleilaag 1");
 		assertTrue(bulkDensityExchangeItemObject instanceof DoubleExchangeItem);
 		DoubleExchangeItem bulkDensityExchangeItem = (DoubleExchangeItem) bulkDensityExchangeItemObject;
 		assertEquals("The bulk density is 0.998754", 0.998754, bulkDensityExchangeItem.getValue());
 
-		Object porosityExchangeItemObject = wandaSeawatModelRunIniFileDataObject.getDataObjectExchangeItem("h1GeoformationsPorosity");
+		Object porosityExchangeItemObject = wandaSeawatModelRunIniFileDataObject.getDataObjectExchangeItem("HTGEOFIL H1.Geoformations.Porosity formation.kleilaag 1");
 		assertTrue(porosityExchangeItemObject instanceof DoubleExchangeItem);
 		DoubleExchangeItem porosityExchangeItem = (DoubleExchangeItem) porosityExchangeItemObject;
-		assertEquals("The bulk density is 0.6", 0.6, porosityExchangeItem.getValue());
+		assertEquals("The porosity is 0.6", 0.6, porosityExchangeItem.getValue());
 
-		Object maxYearlyWaterVolumeExchangeItemObject = wandaSeawatModelRunIniFileDataObject.getDataObjectExchangeItem("h1MaxYearlyWaterVolume");
-		assertTrue(maxYearlyWaterVolumeExchangeItemObject instanceof IntExchangeItem);
-		IntExchangeItem maxYearlyWaterVolumeExchangeItem = (IntExchangeItem) maxYearlyWaterVolumeExchangeItemObject;
-		assertEquals("The bulk density is 65484", 65484, maxYearlyWaterVolumeExchangeItem.getValue());
+		Object maxYearlyWaterVolumeExchangeItemObject = wandaSeawatModelRunIniFileDataObject.getDataObjectExchangeItem("HTGEOFIL H1.Max yearly water volume");
+		assertTrue(maxYearlyWaterVolumeExchangeItemObject instanceof DoubleExchangeItem);
+		DoubleExchangeItem maxYearlyWaterVolumeExchangeItem = (DoubleExchangeItem) maxYearlyWaterVolumeExchangeItemObject;
+		assertEquals("The max yearly water volume is 65484", 65484.0, maxYearlyWaterVolumeExchangeItem.getValue());
+		
+		porosityExchangeItem.setValue(5.4);
+		wandaSeawatModelRunIniFileDataObject.finish();
+
+		try (Stream<String> stream = Files.lines(file.toPath())) {
+			String lines = stream.collect(Collectors.joining("\n"));
+			assertTrue("Updated value present in ini file", lines.contains("HTGEOFIL H1.Geoformations.Porosity formation.kleilaag 1 = 5.4"));
+		}
 	}
 }
