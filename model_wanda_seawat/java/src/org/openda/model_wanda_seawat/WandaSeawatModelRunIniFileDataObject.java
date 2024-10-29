04a05e09fb118494183a860710cea3011d2a53d4
pelgrim <erik.pelgrim@deltares.nl>
2024-10-29 17:29:21 +0100

    Allow slashes in file paths, skip references to .csv files

--- model_wanda_seawat/java/src/org/openda/model_wanda_seawat/WandaSeawatModelRunIniFileDataObject.java
+++ model_wanda_seawat/java/src/org/openda/model_wanda_seawat/WandaSeawatModelRunIniFileDataObject.java
@@ -10,12 +10,14 @@ import org.openda.interfaces.IExchangeItem;
 import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class WandaSeawatModelRunIniFileDataObject extends AbstractDataObject {
 	private static final String SECTION_RUN = "run";
 	private static final String OPTION_START_DATE_TIME = "startDateTime";
 	private static final String OPTION_END_DATE_TIME = "endDateTime";
 	private static final String SECTION_PARAMETERS = "parameters";
+	public static final String datePattern = "yyyyMMddHHmmss";
 	private File file;
 	private Ini ini;
 
@@ -25,12 +27,13 @@ public class WandaSeawatModelRunIniFileDataObject extends AbstractDataObject {
 			throw new RuntimeException("Exchange items not yet initialized.");
 		}
 		DoubleExchangeItem startTimeExchangeItem = (DoubleExchangeItem) exchangeItems.get(OPTION_START_DATE_TIME);
-		ini.put(SECTION_RUN, OPTION_START_DATE_TIME, startTimeExchangeItem.getValue());
+		ini.put(SECTION_RUN, OPTION_START_DATE_TIME, TimeUtils.mjdToString(startTimeExchangeItem.getValue(), datePattern));
 		DoubleExchangeItem endTimeExchangeItem = (DoubleExchangeItem) exchangeItems.get(OPTION_END_DATE_TIME);
-		ini.put(SECTION_RUN, OPTION_END_DATE_TIME, endTimeExchangeItem.getValue());
+		ini.put(SECTION_RUN, OPTION_END_DATE_TIME, TimeUtils.mjdToString(endTimeExchangeItem.getValue(), datePattern));
 		Profile.Section optionsFromParameters = ini.get(SECTION_PARAMETERS);
 		for (String option : optionsFromParameters.keySet()) {
-			DoubleExchangeItem parameterExchangeItem = (DoubleExchangeItem) exchangeItems.get(option); 
+			DoubleExchangeItem parameterExchangeItem = (DoubleExchangeItem) exchangeItems.get(option);
+			if (parameterExchangeItem == null) continue;
 			ini.put(SECTION_PARAMETERS, option, parameterExchangeItem.getValue());
 		}
 		try {
@@ -44,6 +47,7 @@ public class WandaSeawatModelRunIniFileDataObject extends AbstractDataObject {
 	public void initialize(File workingDir, String[] arguments) {
 		file = new File(workingDir, arguments[0]);
 		ini = new Ini();
+		ini.getConfig().setEscape(false);
 		try {
 			ini.load(file);
 			dateTimeExchangeItem(ini, OPTION_START_DATE_TIME, OPTION_START_DATE_TIME);
@@ -66,6 +70,7 @@ public class WandaSeawatModelRunIniFileDataObject extends AbstractDataObject {
 	
 	private void doubleExchangeItem(Ini ini, String optionName, String id) {
 		String valueString = ini.get(SECTION_PARAMETERS, optionName);
+		if (valueString.endsWith(".csv")) return;
 		DoubleExchangeItem doubleExchangeItem = new DoubleExchangeItem(id, IExchangeItem.Role.InOut, Double.parseDouble(valueString));
 		exchangeItems.putIfAbsent(id, doubleExchangeItem);
 	}
