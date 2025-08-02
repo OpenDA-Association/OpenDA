e9e58f01f60eee82bc0501e92836baa750147f73
robot144 <robot144@github.com>
2025-08-02 08:55:44 +0200

    Refine cleaning up outputs. And adapt course text to current status where some input files are already there and do not have to be created by the user.

--- course/exercise_black_box_calibration_polution/clean.sh
+++ course/exercise_black_box_calibration_polution/clean.sh
@@ -4,7 +4,7 @@
 #
 
 echo "Removing working directories"
-rm -rf stochModel/output/work[0-9]*
+rm -rf work/work[0-9]*
 rm -f *_results.m
 rm -f *_results.py
 rm -f openda_logfile.txt
@@ -12,4 +12,7 @@ rm -f *.orp
 rm -f OpenDATimings_*
 rm -f restart_*.xml
 rm -f *.png
-rm -f *.pyc
+rm -rf __pycache__
+rm -rf original_model/maps
+rm -rf original_model/restart
+rm -f original_model/c*_loc*.csv
