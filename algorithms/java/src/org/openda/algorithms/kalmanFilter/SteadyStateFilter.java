bc01b77bd91587e333dddfe74d9be0749f8977f1
Erik Pelgrim <erik.pelgrim@deltares.nl>
2025-12-03 14:42:20 +0100

    Fix index out of bounds for asynchrone SteadyStateFilter with estimateMissingObservations #436

--- algorithms/java/src/org/openda/algorithms/kalmanFilter/SteadyStateFilter.java
+++ algorithms/java/src/org/openda/algorithms/kalmanFilter/SteadyStateFilter.java
@@ -325,6 +325,7 @@ public class SteadyStateFilter extends AbstractSequentialAlgorithm {
 		private final String[] availableObsGainVectorIdArray;
 		private final HashMap<String, IVector> gainVectors;
 		ArrayList<Integer> missingObservationIndices = new ArrayList<>();
+		ArrayList<String> missingObservationGainVectorIds = new ArrayList<>();
 		ArrayList<Integer> availableObservationIndicesGainStorage = new ArrayList<>();
 		ArrayList<Integer> availableObservationIndicesObserver = new ArrayList<>();
 		private int numberOfMissingObservations;
@@ -350,6 +351,7 @@ public class SteadyStateFilter extends AbstractSequentialAlgorithm {
 				}
 				System.out.printf("Observation for %s missing%n", gainVectorId);
 				this.missingObservationIndices.add(i);
+				this.missingObservationGainVectorIds.add(gainVectorId);
 			}
 		}
 
@@ -395,8 +397,7 @@ public class SteadyStateFilter extends AbstractSequentialAlgorithm {
 			for (int i = 0; i < this.missingObservationIndices.size(); i++) {
 				Integer missingObservationIndex = this.missingObservationIndices.get(i);
 				String missingObservationId = gainStorageObservationIdsArray[missingObservationIndex];
-				// Use relative time stamps in ids to distinguish between observations for asynchronous filtering
-				String gainVectorId = missingObservationId + ":" + Math.round(obsTimeOffsets[i] * 24.0 * 3600.0); //conversion to seconds
+				String gainVectorId = missingObservationGainVectorIds.get(i);
 				Results.putProgression("processing obs " + gainVectorId + "\n");
 				// add to analysis increment for this obs
 				if (this.gainVectors.containsKey(gainVectorId)) {
