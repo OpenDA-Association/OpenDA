ecd64f73c1058facab151db72e6859a383788d3b
Michael Topsom <119038284+michael-topsom-deltares@users.noreply.github.com>
2026-01-29 16:09:55 +0100

    FEWS-33815: Added test, control file, and exchange item

--- model_hec_hms/java/src/org/openda/model_hec_hms/ControlFile.java
+++ model_hec_hms/java/src/org/openda/model_hec_hms/ControlFile.java
@@ -1,33 +1,202 @@
 package org.openda.model_hec_hms;
 
+import org.openda.exchange.timeseries.TimeUtils;
 import org.openda.interfaces.IDataObject;
 import org.openda.interfaces.IExchangeItem;
 
-import java.io.File;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class ControlFile implements IDataObject {
+	private static final String[] EMPTY_STRING_ARRAY = new String[0];
+	private File workingDirectory;
+	private String filename = null;
+
+	private String controlFileId = null;
+	private final Map<String, IExchangeItem> exchangeItems = new HashMap<>();
+	private String timeZoneId = null;
+
+	private final Map<Integer, String> lines = new LinkedHashMap<>();
+	private final Map<Integer, String> valuesToUpdate = new HashMap<>();
+
 	@Override
 	public String[] getExchangeItemIDs() {
-		return new String[0];
+		return new String[]{"ControlFileExchangeItem"};
 	}
 
 	@Override
 	public String[] getExchangeItemIDs(IExchangeItem.Role role) {
-		return new String[0];
+		return exchangeItems.keySet().toArray(EMPTY_STRING_ARRAY);
 	}
 
 	@Override
 	public IExchangeItem getDataObjectExchangeItem(String exchangeItemID) {
-		return null;
+		return exchangeItems.get(exchangeItemID);
 	}
 
 	@Override
 	public void finish() {
+		File outputFile = new File(workingDirectory, filename);
 
+		ZoneId zoneId = ZoneId.of(timeZoneId);
+		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd MMMM yyyy").withZone(zoneId);
+		DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm").withZone(zoneId);
+
+		IExchangeItem exchangeItem = exchangeItems.get(controlFileId);
+		double[] times = exchangeItem.getTimes();
+
+		ZoneId utcZoneId = ZoneId.of("UTC");
+
+		Date startDate = TimeUtils.mjdToDate(times[0]);
+		startDate.setHours((int) Math.round((times[0] % 1) * 24));
+		LocalDateTime startDateTime = startDate.toInstant().atZone(utcZoneId).toLocalDateTime();
+
+		Date endDate = TimeUtils.mjdToDate(times[1]);
+		endDate.setHours((int) Math.round((times[1] % 1) * 24));
+		LocalDateTime endDateTime = endDate.toInstant().atZone(utcZoneId).toLocalDateTime();
+
+		try (FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+			 OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+			 BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter)) {
+			int lineNumber = 0;
+			for (Map.Entry<Integer, String> line : lines.entrySet()) {
+				if (valuesToUpdate.containsKey(lineNumber)) {
+					String label = line.getValue().split(":")[0];
+					bufferedWriter.write(label);
+
+					String value = null;
+
+					switch (label.trim()) {
+						case "Start Date":
+							value = dateFormatter.format(startDateTime);
+							break;
+						case "Start Time":
+							value = timeFormatter.format(startDateTime);
+							break;
+						case "End Date":
+							value = dateFormatter.format(endDateTime);
+							break;
+						case "End Time":
+							value = timeFormatter.format(endDateTime);
+							break;
+						default:
+							// Do nothing with other labels
+					}
+
+					bufferedWriter.write(": " + value);
+				} else {
+					bufferedWriter.write(line.getValue());
+				}
+				bufferedWriter.newLine();
+
+				lineNumber++;
+			}
+		} catch (IOException ioException) {
+			throw new RuntimeException(ioException);
+		}
 	}
 
 	@Override
-	public void initialize(File workingDir, String[] arguments) {
+	public void initialize(File workingDirectory, String[] arguments) {
+		this.workingDirectory = workingDirectory;
 
+		File inputFile = getInputFile(arguments);
+		processInputFile(inputFile);
+	}
+
+	private File getInputFile(String[] arguments) {
+		if (null == arguments || 1 != arguments.length) {
+			throw new RuntimeException("Expected filename as only argument.");
+		}
+
+		filename = arguments[0];
+
+		File inputFile = new File(workingDirectory, filename);
+
+		try {
+			if (!inputFile.exists()) {
+				throw new RuntimeException("File: " + inputFile.getCanonicalFile() + " does not exist.");
+			}
+
+			if (!inputFile.isFile()) {
+				throw new RuntimeException("File: " + inputFile.getCanonicalFile() + " is not a file.");
+			}
+		} catch (IOException ioException) {
+			throw new RuntimeException(ioException);
+		}
+		return inputFile;
+	}
+
+	private void processInputFile(File inputFile) {
+		try (FileInputStream fileInputStream = new FileInputStream(inputFile);
+			 InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+			 BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+			String line = bufferedReader.readLine();
+			int lineNumber = 0;
+
+			String startDate = null;
+			String startTime = null;
+			String endDate = null;
+			String endTime = null;
+
+			while (null != line) {
+				lines.put(lineNumber, line);
+
+				if (line.contains(":")) {
+					String[] labelAndValue = line.split(":");
+					String label = labelAndValue[0].trim();
+
+					switch (label) {
+						case "Control":
+							controlFileId = labelAndValue[1].trim();
+							break;
+						case "Time Zone ID":
+							timeZoneId = labelAndValue[1].trim();
+							break;
+						case "Start Date":
+							valuesToUpdate.put(lineNumber, label);
+							startDate = labelAndValue[1].trim();
+							break;
+						case "Start Time":
+							valuesToUpdate.put(lineNumber, label);
+							startTime = labelAndValue[1].trim() + ":" + labelAndValue[2].trim();
+							break;
+						case "End Date":
+							valuesToUpdate.put(lineNumber, label);
+							endDate = labelAndValue[1].trim();
+							break;
+						case "End Time":
+							valuesToUpdate.put(lineNumber, label);
+							endTime = labelAndValue[1].trim() + ":" + labelAndValue[2].trim();
+							break;
+						default:
+							// Do nothing with other labels
+					}
+				}
+
+				line = bufferedReader.readLine();
+				lineNumber++;
+			}
+
+			DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd MMMM yyyyHH:mmVV");
+			ZonedDateTime startDateTime = ZonedDateTime.parse(startDate + startTime + timeZoneId, dateTimeFormatter);
+			ZonedDateTime endDateTime = ZonedDateTime.parse(endDate + endTime + timeZoneId, dateTimeFormatter);
+
+			double[] times = new double[2];
+
+			long startDateTimeMillis = startDateTime.toInstant().toEpochMilli();
+			times[0] = TimeUtils.date2Mjd(new Date(startDateTimeMillis));
+			long endDateTimeMillis = endDateTime.toInstant().toEpochMilli();
+			times[1] = TimeUtils.date2Mjd(new Date(endDateTimeMillis));
+
+			exchangeItems.putIfAbsent(controlFileId, new ControlFileExchangeItem(controlFileId, times));
+		} catch (IOException ioException) {
+			throw new RuntimeException(ioException);
+		}
 	}
 }
--- model_hec_hms/java/test/org/openda/model_hec_hms/ControlFileTest.java
+++ model_hec_hms/java/test/org/openda/model_hec_hms/ControlFileTest.java
@@ -1,6 +1,65 @@
 package org.openda.model_hec_hms;
 
 import junit.framework.TestCase;
+import org.openda.interfaces.IDataObject;
+import org.openda.interfaces.IExchangeItem;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
 
 public class ControlFileTest extends TestCase {
+	private static final File TEST_RESOURCES_DIRECTORY = new File("java/testResources");
+	private static final double DELTA = 0.001;
+
+	public void testControlFile() throws IOException {
+		// Given input file
+		File temporaryInputFile = File.createTempFile("temp-exchange-items", ".control");
+		File sourceInputFile = new File(TEST_RESOURCES_DIRECTORY, "exchange-items.control");
+		try {
+			Files.copy(sourceInputFile.toPath(), temporaryInputFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+			// Given control file
+			IDataObject dataObject = new ControlFile();
+			String[] args = {temporaryInputFile.getName()};
+
+			// When initialized
+			dataObject.initialize(new File(temporaryInputFile.getParent()), args);
+
+			// Then exchange items are found
+			String[] exchangeItemIds = dataObject.getExchangeItemIDs();
+			assertEquals(1, exchangeItemIds.length);
+
+			// Then exchange items contains values for:
+			IExchangeItem exchangeItem = dataObject.getDataObjectExchangeItem("Control_File");
+			assertNotNull(exchangeItem);
+			assertTrue(exchangeItem instanceof ControlFileExchangeItem);
+			ControlFileExchangeItem controlFileExchangeItem = (ControlFileExchangeItem) exchangeItem;
+
+			// Then the exchange item has an array of values
+			double[] values = controlFileExchangeItem.getValuesAsDoubles();
+			assertEquals(2, values.length);
+			assertEquals(60611.958, values[0], DELTA);
+			assertEquals(60612.166, values[1], DELTA);
+
+			// When the values are updated
+			double[] newTimes = new double[2];
+			newTimes[0] = 60637.875; // 23 November 2024 21:00 UTC
+			newTimes[1] = 60678.25; // 03 January 2025 06:00 UTC
+
+			exchangeItem.setTimes(newTimes);
+
+			// And the file is output
+			dataObject.finish();
+
+			// Then the values are updated
+			List<String> expectedLines = Files.readAllLines(new File(TEST_RESOURCES_DIRECTORY, "expected-exchange-items.control").toPath());
+			List<String> actualLines = Files.readAllLines(temporaryInputFile.toPath());
+			assertEquals(expectedLines, actualLines);
+		} finally {
+			Files.deleteIfExists(temporaryInputFile.toPath());
+		}
+	}
 }
